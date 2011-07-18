175161ca8de19cddd7de9320b29d4b61bba73e98
strentini <none@none>
2011-07-18 17:05:11 +0200

    OLAT-6245 : collect evidence achievement right from the popup (efficiency statement/leistungsnachweis) new artefact-collect-link, wizard opens right within the popup
    FXOLAT-193
    
    --HG--
    branch : uzhFixes711

--- olat3/webapp/WEB-INF/src/org/olat/course/assessment/EfficiencyStatementController.java
+++ olat3/webapp/WEB-INF/src/org/olat/course/assessment/EfficiencyStatementController.java
@@ -21,11 +21,15 @@
 
 package org.olat.course.assessment;
 
+import java.util.Date;
 import java.util.List;
 
+import org.olat.core.CoreSpringFactory;
 import org.olat.core.commons.fullWebApp.LayoutMain3ColsController;
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.components.Component;
+import org.olat.core.gui.components.link.Link;
+import org.olat.core.gui.components.link.LinkFactory;
 import org.olat.core.gui.components.velocity.VelocityContainer;
 import org.olat.core.gui.control.Controller;
 import org.olat.core.gui.control.Event;
@@ -34,6 +38,12 @@ import org.olat.core.gui.control.controller.MainLayoutBasicController;
 import org.olat.core.gui.control.generic.messages.MessageUIFactory;
 import org.olat.core.id.Roles;
 import org.olat.core.util.StringHelper;
+import org.olat.core.util.vfs.VFSContainer;
+import org.olat.course.assessment.portfolio.EfficiencyStatementArtefact;
+import org.olat.portfolio.EPArtefactHandler;
+import org.olat.portfolio.PortfolioModule;
+import org.olat.portfolio.model.artefacts.AbstractArtefact;
+import org.olat.portfolio.ui.artefacts.collect.ArtefactWizzardStepsController;
 import org.olat.user.UserManager;
 import org.olat.user.propertyhandlers.UserPropertyHandler;
 
@@ -50,6 +60,14 @@ public class EfficiencyStatementController extends MainLayoutBasicController {
 	private VelocityContainer userDataVC;
 	private static final String usageIdentifyer = EfficiencyStatementController.class.getCanonicalName();
 	
+	private EfficiencyStatement efficiencyStatement;
+	
+	//to collect the eff.Statement as artefact
+	private Link collectArtefactLink;
+	private PortfolioModule portfolioModule;
+	// the collect-artefact-wizard
+	private Controller ePFCollCtrl; 
+
 	/**
 	 * Constructor
 	 * @param wControl
@@ -67,6 +85,8 @@ public class EfficiencyStatementController extends MainLayoutBasicController {
 		Component content = null;
 		
 		if (efficiencyStatement != null) {
+			this.efficiencyStatement = efficiencyStatement;
+			
 			//extract efficiency statement data
 			//fallback translation for user properties 
 			setTranslator(UserManager.getInstance().getPropertyHandlerTranslator(getTranslator()));		
@@ -84,6 +104,14 @@ public class EfficiencyStatementController extends MainLayoutBasicController {
 			Controller identityAssessmentCtr = new IdentityAssessmentOverviewController(ureq, wControl, efficiencyStatement.getAssessmentNodes());
 			listenTo(identityAssessmentCtr);//dispose it when this one is disposed
 			userDataVC.put("assessmentOverviewTable", identityAssessmentCtr.getInitialComponent());
+			
+			//add link to collect efficiencyStatement as artefact
+			portfolioModule = (PortfolioModule) CoreSpringFactory.getBean("portfolioModule");
+			EPArtefactHandler<?> artHandler = portfolioModule.getArtefactHandler(EfficiencyStatementArtefact.ARTEFACT_TYPE);
+			if(portfolioModule.isEnabled() && artHandler != null && artHandler.isEnabled()) {
+				collectArtefactLink = LinkFactory.createCustomLink("collectArtefactLink", "collectartefact", "", Link.NONTRANSLATED, userDataVC, this);
+				collectArtefactLink.setCustomEnabledLinkCSS("b_eportfolio_add_again");
+			}
 			
 			content = userDataVC;
 		} else {
@@ -106,9 +134,35 @@ public class EfficiencyStatementController extends MainLayoutBasicController {
 	 */
 	@SuppressWarnings("unused")
 	public void event(UserRequest ureq, Component source, Event event) {
-		// nothing to catch
+		if(source.equals(collectArtefactLink)){
+			popupArtefactCollector(ureq);
+		}
+		
 	}
 
+	/**
+	 * opens the collect-artefact wizard 
+	 * 
+	 * @param ureq
+	 */
+	private void popupArtefactCollector(UserRequest ureq) {
+		EPArtefactHandler<?> artHandler = portfolioModule.getArtefactHandler(EfficiencyStatementArtefact.ARTEFACT_TYPE);
+		if(artHandler != null && artHandler.isEnabled()) {
+			AbstractArtefact artefact = artHandler.createArtefact();
+			artefact.setAuthor(getIdentity());//only author can create artefact
+			//no business path becouse we cannot launch an efficiency statement
+			artefact.setCollectionDate(new Date());
+			artefact.setTitle(translate("artefact.title", new String[]{efficiencyStatement.getCourseTitle()}));
+			artHandler.prefillArtefactAccordingToSource(artefact, efficiencyStatement);
+			ePFCollCtrl = new ArtefactWizzardStepsController(ureq, getWindowControl(), artefact, (VFSContainer)null);
+			listenTo(ePFCollCtrl);
+			
+			//set flag for js-window-resizing (see velocity)
+			userDataVC.contextPut("collectwizard", true);
+		}
+	}
+	
+	
 	/**
 	 * @see org.olat.core.gui.control.DefaultController#doDispose(boolean)
 	 */
