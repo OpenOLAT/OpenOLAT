bcf535e2bc779e2f076511f8e50085a6826c3b2c
srosse <none@none>
2011-12-09 14:28:38 +0100

    FXOLAT-358: move frentix copyrighted headers to openolat.org in org.olat

--- src/main/java/org/olat/group/ui/main/BGTableItem.java
+++ src/main/java/org/olat/group/ui/main/BGTableItem.java
@@ -1,12 +1,11 @@
 /**
- * OLAT - Online Learning and Training<br>
- * http://www.olat.org
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); <br>
  * you may not use this file except in compliance with the License.<br>
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
  * <p>
  * Unless required by applicable law or agreed to in writing,<br>
  * software distributed under the License is distributed on an "AS IS" BASIS, <br>
@@ -14,8 +13,8 @@
  * See the License for the specific language governing permissions and <br>
  * limitations under the License.
  * <p>
- * Copyright (c) frentix GmbH<br>
- * http://www.frentix.com<br>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
  * <p>
  */
 package org.olat.group.ui.main;
