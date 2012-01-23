91adc6636042a3ff276f4db563272218e8015b89
strentini <none@none>
2012-01-23 09:43:44 +0100

    OO-1  :  print-styles and fixes

--- src/main/webapp/static/themes/compiletheme.sh
+++ src/main/webapp/static/themes/compiletheme.sh
@@ -6,10 +6,13 @@
 
 echo "compiling SASS..."
 sass --version
-sass --style extended  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile .
+
+#sass --style extended  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile .
 
 #sass --style extended  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile bzg
 #sass --style extended  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile zag
+sass --style extended  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile kantiwohlen
+
 
 
 echo "done"
