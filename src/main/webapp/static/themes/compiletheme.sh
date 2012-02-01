9de9dfa96845d62dc64ea3dcf667d4dee3f5df21
strentini <none@none>
2012-02-01 14:51:49 +0100

    OO-1, OO-2 :  fix single-page edit icon in iframe

--- src/main/webapp/static/themes/compiletheme.sh
+++ src/main/webapp/static/themes/compiletheme.sh
@@ -11,7 +11,16 @@ sass --version
 
 sass --style $style --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile .
 
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile hfgs
 
-
+: '
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile bzg
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile zag
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile kantiwohlen
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile olatpro
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile business
+sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile pedsibd
+'
 echo "done"
+
 
