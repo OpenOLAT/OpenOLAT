5c1af39f80d158d74263b03d96368f6d5baeae00
strentini <none@none>
2012-02-24 08:50:18 +0100

    OO-1 : fix impressum-link in topnav, fix line-through on deleted coursenodes in coruseditor
    --HG--
    branch : 80-patch

--- src/main/webapp/static/themes/compiletheme.sh
+++ src/main/webapp/static/themes/compiletheme.sh
@@ -11,16 +11,6 @@ sass --version
 
 sass --style $style --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile .
 
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile hfgs
-
-: '
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile bzg
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile zag
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile kantiwohlen
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile olatpro
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile business
-sass --style $style  --update --load-path openolat openolat/all openolat/all/modules openolat/print openolat/mobile pedsibd
-'
 echo "done"
 
 
