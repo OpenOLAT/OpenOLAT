81c504fe39b3482d284975e83abcb74cbdbfaa00
gnaegi <none@none>
2012-08-27 17:39:06 +0200

    non-jira: update version to 8.2.b

--- src/test/java/org/olat/test/ArquillianDeployments.java
+++ src/test/java/org/olat/test/ArquillianDeployments.java
@@ -35,7 +35,7 @@ public class ArquillianDeployments {
 	public static final String WEBAPP    = "src/main/webapp";
 	public static final String WEBINF    = "src/main/webapp/WEB-INF";
 	public static final String TEST_RSRC = "src/test/resources";
-	public static final String LIB_DIR   = "target/openolat-lms-8.1-SNAPSHOT/WEB-INF/lib";
+	public static final String LIB_DIR   = "target/openolat-lms-8.2-SNAPSHOT/WEB-INF/lib";
 
 	
 	public static WebArchive createDeployment() {
