c6e0de18a5eccc604327283e927b7462860e1536
srosse <none@none>
2012-10-15 08:46:36 +0200

    OO-259: make the window for the link a little wider

--- src/main/java/org/olat/core/gui/components/form/flexible/impl/elements/richText/_static/js/tinymce/themes/advanced/editor_template_src.js
+++ src/main/java/org/olat/core/gui/components/form/flexible/impl/elements/richText/_static/js/tinymce/themes/advanced/editor_template_src.js
@@ -1179,8 +1179,8 @@
 
 			ed.windowManager.open({
 				url : this.url + '/link.htm',
-				width : 310 + parseInt(ed.getLang('advanced.link_delta_width', 0)),
-				height : 200 + parseInt(ed.getLang('advanced.link_delta_height', 0)),
+				width : 360 + parseInt(ed.getLang('advanced.link_delta_width', 0)),
+				height : 210 + parseInt(ed.getLang('advanced.link_delta_height', 0)),
 				inline : true
 			}, {
 				theme_url : this.url
