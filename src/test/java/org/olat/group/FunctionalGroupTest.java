7590269bfaf698063ccfbea4a030e40242fc1eca
Joël Krähemann <joel.kraehemann@frentix.com>
2012-11-14 15:23:52 +0100

    OO-349: implemented group members test case

--- src/test/java/org/olat/group/FunctionalGroupTest.java
+++ src/test/java/org/olat/group/FunctionalGroupTest.java
@@ -22,7 +22,6 @@ package org.olat.group;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.List;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -42,6 +41,7 @@ import org.olat.util.FunctionalGroupsSiteUtil;
 import org.olat.util.FunctionalGroupsSiteUtil.GroupOptions;
 import org.olat.util.FunctionalGroupsSiteUtil.GroupTools;
 import org.olat.util.FunctionalGroupsSiteUtil.GroupsTabAction;
+import org.olat.util.FunctionalGroupsSiteUtil.MembersConfiguration;
 import org.olat.util.FunctionalUtil;
 import org.olat.util.FunctionalUtil.WaitLimitAttribute;
 import org.olat.util.FunctionalVOUtil;
@@ -190,8 +190,67 @@ public class FunctionalGroupTest {
 	@Ignore
 	@Test
 	@RunAsClient
-	public void checkConfigureMembers(){
+	public void checkConfigureMembers(@Drone @Tutor1 DefaultSelenium tutor0,
+			@Drone @Student1 DefaultSelenium student0)
+					throws IOException, URISyntaxException
+	{
+		/*
+		 * test setup
+		 */
+		/* create users */
+		int tutorCount = 1;
+			
+		final UserVO[] tutors = new UserVO[tutorCount];
+		functionalVOUtil.createTestUsers(deploymentUrl, tutorCount).toArray(tutors);
 		
+		int userCount = 1;
+		
+		final UserVO[] students = new UserVO[userCount]; 
+		functionalVOUtil.createTestUsers(deploymentUrl, userCount).toArray(students);
+		
+		/* create group via REST */
+		final GroupVO[] groups = new GroupVO[tutorCount];
+		functionalVOUtil.createTestCourseGroups(deploymentUrl, tutorCount).toArray(groups);
+		
+		functionalVOUtil.addOwnerToGroup(deploymentUrl, groups[0], tutors[0]);
+		
+		/*
+		 * test case
+		 */
+		Assert.assertTrue(functionalUtil.login(tutor0, tutors[0].getLogin(), tutors[0].getPassword(), true));
+		
+		/* open group */
+		Assert.assertTrue(functionalGroupsSiteUtil.openMyGroup(tutor0, groups[0].getName()));
+		
+		/* apply members configuration */
+		MembersConfiguration[] conf = new MembersConfiguration[]{
+				MembersConfiguration.CAN_SEE_PARTICIPANTS,
+				MembersConfiguration.ALL_CAN_SEE_PARTICIPANTS,
+				MembersConfiguration.ALL_CAN_DOWNLOAD_LIST_OF_MEMBERS
+		};
+		
+		Assert.assertTrue(functionalGroupsSiteUtil.applyMembersConfiguration(tutor0, conf));
+		
+		/* logout */
+		functionalUtil.idle(tutor0);
+		functionalUtil.logout(tutor0);
+		
+		/*
+		 * verify
+		 */
+		Assert.assertTrue(functionalUtil.login(student0, students[0].getLogin(), students[0].getPassword(), true));
+		
+		Assert.assertTrue(functionalGroupsSiteUtil.openPublishedGroup(student0, groups[0].getName()));
+
+		Assert.assertTrue(functionalGroupsSiteUtil.openGroupsTabActionByMenuTree(student0, GroupsTabAction.GROUPS));
+		
+		StringBuffer selectorBuffer = new StringBuffer();
+		
+		//TODO:JK: implement me
+		
+		/* logout */
+		functionalUtil.idle(student0);
+		functionalUtil.logout(student0);
 	}
 
 	@Ignore
