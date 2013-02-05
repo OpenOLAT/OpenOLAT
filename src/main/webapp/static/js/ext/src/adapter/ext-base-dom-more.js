2c295792fee71c8e6e66c3808da72cf0e7668d0d
srosse <none@none>
2013-02-05 10:29:39 +0100

    OO-531: remove Ext and script.aculo.us

--- src/main/webapp/static/js/ext/src/adapter/ext-base-dom-more.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Ext JS Library 3.4.0
- * Copyright(c) 2006-2011 Sencha Inc.
- * licensing@sencha.com
- * http://www.sencha.com/license
- */
-Ext.lib.Dom.getRegion = function(el) {
-    return Ext.lib.Region.getRegion(el);
-};
