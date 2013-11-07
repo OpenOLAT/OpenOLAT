d2795990b84614ce2ba66432d1400018e7088c68
srosse <none@none>
2013-11-07 08:52:59 +0100

    OO-751: copy the right SQL statement for oracle and postgresql in the right file

--- src/main/resources/database/postgresql/alter_9_0_0_to_9_1_0.sql
+++ src/main/resources/database/postgresql/alter_9_0_0_to_9_1_0.sql
@@ -3,13 +3,4 @@ alter table o_bs_authentication add column salt varchar(255) default null;
 alter table o_bs_authentication add column hashalgorithm varchar(16) default null;
 
 
-alter table o_info_message add message_copy varchar2(4000);
-update o_info_message set message_copy = message;
-Commit;
-update o_info_message set message = null;
-commit;
-alter table o_info_message modify message long;
-alter table o_info_message modify message clob;
-update o_info_message set message = message_copy;
-Commit;
-alter table o_info_message drop column message_copy;
+alter table o_info_message alter column message type text;
