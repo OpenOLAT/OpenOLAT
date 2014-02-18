069c1f4f348fc28ebefe84087091c0a70c3fd494
srosse <none@none>
2014-02-18 12:12:03 +0100

    OO-980: sort the catalog list in controller

--- src/main/java/org/olat/catalog/CatalogManager.java
+++ src/main/java/org/olat/catalog/CatalogManager.java
@@ -183,8 +183,7 @@ public class CatalogManager extends BasicManager implements UserDataDeletable, I
 		StringBuilder sb = new StringBuilder();
 		sb.append("select cei from ").append(CatalogEntryImpl.class.getName()).append(" as cei ")
 		  .append(" inner join fetch cei.ownerGroup as ownerGroup")
-		  .append(" where cei.type=").append(CatalogEntry.TYPE_NODE)
-		  .append(" order by cei.name");
+		  .append(" where cei.type=").append(CatalogEntry.TYPE_NODE);
 		
 		return dbInstance.getCurrentEntityManager()
 				.createQuery(sb.toString(), CatalogEntry.class)
