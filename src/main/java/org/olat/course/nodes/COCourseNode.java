87d3ad724101aa8c399f1e897f09064513dc476e
Dirk Furrer <none@none>
2014-12-11 12:55:44 +0100

    OO-998: fixed config-validation

--- src/main/java/org/olat/course/nodes/COCourseNode.java
+++ src/main/java/org/olat/course/nodes/COCourseNode.java
@@ -217,20 +217,24 @@ public class COCourseNode extends AbstractAccessableCourseNode {
         @SuppressWarnings("unchecked")
         List<String> emailList = (List<String>) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOADRESSES);
         boolean isValid = (emailList != null && emailList.size() > 0);
-//        Boolean email2coaches = getModuleConfiguration().getBooleanEntry(COEditController.CONFIG_KEY_EMAILTOCOACHES_ALL) || getModuleConfiguration().getBooleanEntry(COEditController.CONFIG_KEY_EMAILTOCOACHES_COURSE);
-//        Boolean email2partips = getModuleConfiguration().getBooleanEntry(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_ALL) || getModuleConfiguration().getBooleanEntry(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_COURSE);
-//        isValid = isValid || (email2coaches != null && email2coaches.booleanValue());
-//        isValid = isValid || (email2partips != null && email2partips.booleanValue());
         Boolean email2owners = getModuleConfiguration().getBooleanEntry(COEditController.CONFIG_KEY_EMAILTOOWNERS);
         isValid = isValid || (email2owners != null && email2owners.booleanValue());
         String email2AreaCoaches = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOCOACHES_AREA);
         isValid = isValid || (!"".equals(email2AreaCoaches) && email2AreaCoaches != null);
         String email2GroupCoaches = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOCOACHES_GROUP);
         isValid = isValid || (!"".equals(email2GroupCoaches) && email2GroupCoaches != null);
+        String email2AllCoaches = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOCOACHES_ALL);
+        isValid = isValid || (!"".equals(email2AllCoaches) && email2AllCoaches != null);
+        String email2CourseCoaches = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOCOACHES_COURSE);
+        isValid = isValid || (!"".equals(email2CourseCoaches) && email2CourseCoaches != null);
         String email2AreaParticipants = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_AREA);
         isValid = isValid || (!"".equals(email2AreaParticipants) && email2AreaParticipants != null);
         String email2GroupParticipants = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_GROUP);
         isValid = isValid || (!"".equals(email2GroupParticipants) && email2GroupParticipants != null);
+        String email2AllParticipants = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_ALL);
+        isValid = isValid || (!"".equals(email2AllParticipants) && email2AllParticipants != null);
+        String email2CourseParticipants = (String) getModuleConfiguration().get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_COURSE);
+        isValid = isValid || (!"".equals(email2CourseParticipants) && email2CourseParticipants != null);
         
         StatusDescription sd = StatusDescription.NOERROR;
         if (!isValid) {
