671eb0e0e98d2335751bbb80cc5a8c8c88dff524
srosse <none@none>
2015-11-16 11:57:11 +0100

    OO-1780: remap the groups after copy of e-mail course element to the right configuration

--- src/main/java/org/olat/course/nodes/co/CORunController.java
+++ src/main/java/org/olat/course/nodes/co/CORunController.java
@@ -95,8 +95,7 @@ public class CORunController extends BasicController {
 		Translator fallback = Util.createPackageTranslator(ContactFormController.class, ureq.getLocale());
 		setTranslator(Util.createPackageTranslator(CORunController.class, ureq.getLocale(), fallback));
 		
-		@SuppressWarnings("unchecked")
-		List<String> emailListConfig = (List<String>) moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOADRESSES);
+		List<String> emailListConfig = moduleConfiguration.getList(COEditController.CONFIG_KEY_EMAILTOADRESSES, String.class);
 		String mSubject = (String) moduleConfiguration.get(COEditController.CONFIG_KEY_MSUBJECT_DEFAULT);
 		String mBody = (String) moduleConfiguration.get(COEditController.CONFIG_KEY_MBODY_DEFAULT);
 
@@ -110,29 +109,25 @@ public class CORunController extends BasicController {
 
 		
 		String participantGroupNames = (String)moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_GROUP);
-		@SuppressWarnings("unchecked")
-		List<Long> participantGroupKeys = (List<Long>) moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_GROUP_ID);
+		List<Long> participantGroupKeys = moduleConfiguration.getList(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_GROUP_ID, Long.class);
 		if(participantGroupKeys == null && StringHelper.containsNonWhitespace(participantGroupNames)) {
 			participantGroupKeys = businessGroupService.toGroupKeys(participantGroupNames, cgm.getCourseEntry());
 		}
 		
 		String participantAreaNames = (String)moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_AREA);
-		@SuppressWarnings("unchecked")
-		List<Long> participantAreaKeys = (List<Long>) moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_AREA_ID);
+		List<Long> participantAreaKeys = moduleConfiguration.getList(COEditController.CONFIG_KEY_EMAILTOPARTICIPANTS_AREA_ID, Long.class);
 		if(participantAreaKeys == null && StringHelper.containsNonWhitespace(participantAreaNames)) {
 			participantAreaKeys = businessGroupService.toGroupKeys(participantAreaNames, cgm.getCourseEntry());
 		}
 		
 		String coachGroupNames = (String)moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOCOACHES_GROUP);
-		@SuppressWarnings("unchecked")
-		List<Long> coachGroupKeys = (List<Long>) moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOCOACHES_GROUP_ID);
+		List<Long> coachGroupKeys = moduleConfiguration.getList(COEditController.CONFIG_KEY_EMAILTOCOACHES_GROUP_ID, Long.class);
 		if(coachGroupKeys == null && StringHelper.containsNonWhitespace(coachGroupNames)) {
 			coachGroupKeys = businessGroupService.toGroupKeys(coachGroupNames, cgm.getCourseEntry());
 		}
 		
 		String coachAreaNames = (String)moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOCOACHES_AREA);
-		@SuppressWarnings("unchecked")
-		List<Long> coachAreaKeys = (List<Long>) moduleConfiguration.get(COEditController.CONFIG_KEY_EMAILTOCOACHES_AREA_IDS);
+		List<Long> coachAreaKeys = moduleConfiguration.getList(COEditController.CONFIG_KEY_EMAILTOCOACHES_AREA_IDS, Long.class);
 		if(coachAreaKeys == null && StringHelper.containsNonWhitespace(coachAreaNames)) {
 			coachAreaKeys = businessGroupService.toGroupKeys(coachAreaNames, cgm.getCourseEntry());
 		}
