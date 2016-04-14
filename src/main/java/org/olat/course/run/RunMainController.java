c7ab03a2ec09e43fcc402dce8dca867e6b41d9a3
srosse <none@none>
2016-04-14 09:00:58 +0200

    OO-1989: if the course element is not found, show a warning

--- src/main/java/org/olat/course/run/RunMainController.java
+++ src/main/java/org/olat/course/run/RunMainController.java
@@ -552,7 +552,7 @@ public class RunMainController extends MainLayoutBasicController implements Gene
 			if (event instanceof OlatCmdEvent) {
 				OlatCmdEvent oe = (OlatCmdEvent) event;
 				String cmd = oe.getCommand();
-				if (cmd.equals(OlatCmdEvent.GOTONODE_CMD)) {
+				if (OlatCmdEvent.GOTONODE_CMD.equals(cmd)) {
 					String subcmd = oe.getSubcommand(); // "69680861018558/node-specific-data";
 					CourseNode identNode;
 					String nodecmd = null;
@@ -563,10 +563,14 @@ public class RunMainController extends MainLayoutBasicController implements Gene
 					} else {
 						identNode = course.getRunStructure().getNode(subcmd);
 					}
-					addLoggingResourceable(LoggingResourceable.wrap(identNode));
-					currentCourseNode = identNode;
-					updateTreeAndContent(ureq, identNode, nodecmd);
-					oe.accept();
+					if(identNode == null) {
+						showWarning("msg.nodenotavailableanymore");
+					} else {
+						addLoggingResourceable(LoggingResourceable.wrap(identNode));
+						currentCourseNode = identNode;
+						updateTreeAndContent(ureq, identNode, nodecmd);
+						oe.accept();
+					}
 				}
 			} else if (event == Event.DONE_EVENT) {
 				// the controller is done.
