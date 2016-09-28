cec947f6a1f97ff8919e941afce8433e19786d21
srosse <none@none>
2016-09-28 16:33:59 +0200

    OO-2219: next button trigger the search if nothing found

--- src/main/java/org/olat/admin/user/UserSearchFlexiController.java
+++ src/main/java/org/olat/admin/user/UserSearchFlexiController.java
@@ -330,6 +330,10 @@ public class UserSearchFlexiController extends FlexiAutoCompleterController {
 		if(source == backLink) {
 			flc.contextPut("noList","false");			
 			flc.contextPut("showButton","false");
+			if(userTableModel != null) {
+				userTableModel.setObjects(new ArrayList<>());
+				tableEl.reset();
+			}
 		} else if(searchButton == source) {
 			if(validateForm(ureq)) {
 				doSearch();
@@ -385,7 +389,7 @@ public class UserSearchFlexiController extends FlexiAutoCompleterController {
 	
 	public List<Identity> getSelectedIdentities() {
 		Set<Integer> index = tableEl.getMultiSelectedIndex();		
-		List<Identity> selectedIdentities =	new ArrayList<Identity>();
+		List<Identity> selectedIdentities =	new ArrayList<>(index.size());
 		for(Integer i : index) {
 			Identity selectedIdentity = userTableModel.getObject(i.intValue());
 			selectedIdentities.add(selectedIdentity);
@@ -393,7 +397,7 @@ public class UserSearchFlexiController extends FlexiAutoCompleterController {
 		return selectedIdentities;
 	}
 	
-	private void doSearch() {
+	public void doSearch() {
 		String login = loginEl.getValue();
 		// build user fields search map
 		Map<String, String> userPropertiesSearch = new HashMap<String, String>();				
@@ -423,6 +427,7 @@ public class UserSearchFlexiController extends FlexiAutoCompleterController {
 	/**
 	 * @see org.olat.core.gui.control.DefaultController#doDispose(boolean)
 	 */
+	@Override
 	protected void doDispose() {
 		// Child controllers auto-disposed by basic controller
 	}
