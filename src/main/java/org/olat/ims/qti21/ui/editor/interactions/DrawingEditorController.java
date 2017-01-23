21ec77b841fc2d41782d36790e1b1bc614b6817f
fkiefer <none@none>
2017-01-23 10:04:00 +0100

    OO-2453 rename context help buttons

--- src/main/java/org/olat/ims/qti21/ui/editor/interactions/DrawingEditorController.java
+++ src/main/java/org/olat/ims/qti21/ui/editor/interactions/DrawingEditorController.java
@@ -100,7 +100,7 @@ public class DrawingEditorController extends FormBasicController {
 
 	@Override
 	protected void initForm(FormItemContainer formLayout, Controller listener, UserRequest ureq) {
-		setFormContextHelp("detail#details_testeditor_fragetypen_drawing");
+		setFormContextHelp("Test editor QTI 2.1 in detail#details_testeditor_fragetypen_drawing");
 		
 		titleEl = uifactory.addTextElement("title", "form.imd.title", -1, itemBuilder.getTitle(), formLayout);
 		titleEl.setMandatory(true);
