714abe5424fc1e0c57802b6cd4bfea6bd5ccb37c
srosse <none@none>
2017-10-04 11:52:09 +0200

    OO-3061: make sure the deleted sessions are committed and reload the finished sessions

--- src/main/java/org/olat/ims/qti21/ui/AssessmentTestDisplayController.java
+++ src/main/java/org/olat/ims/qti21/ui/AssessmentTestDisplayController.java
@@ -280,7 +280,7 @@ public class AssessmentTestDisplayController extends BasicController implements 
             candidateSession.setExploded(true);
             candidateSession = qtiService.updateAssessmentTestSession(candidateSession);
 		} else {
-			qtiService.finishTestSession(candidateSession, testSessionController.getTestSessionState(), assessmentResult,
+			candidateSession = qtiService.finishTestSession(candidateSession, testSessionController.getTestSessionState(), assessmentResult,
          		currentRequestTimestamp, getDigitalSignatureOptions(), getIdentity());
 		}
 	}
@@ -1422,7 +1422,7 @@ public class AssessmentTestDisplayController extends BasicController implements 
 
         /* Handle immediate end of test session */
         if (ended) {
-            qtiService.finishTestSession(candidateSession, testSessionState, assessmentResult,
+        		candidateSession = qtiService.finishTestSession(candidateSession, testSessionState, assessmentResult,
             		timestamp, getDigitalSignatureOptions(), getIdentity());
         } else {
         	TestPart currentTestPart = testSessionController.getCurrentTestPart();
