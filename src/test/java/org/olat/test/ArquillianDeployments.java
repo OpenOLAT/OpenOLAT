21d0afdadd58b291757b8fffdcd4da2c5130347c
srosse <stephane.rosse@frentix.com>
2018-11-23 13:26:28 +0100

    no-jira: prepare for 13.1

--- src/test/java/org/olat/test/ArquillianDeployments.java
+++ src/test/java/org/olat/test/ArquillianDeployments.java
@@ -47,7 +47,7 @@ public class ArquillianDeployments {
 	public static final String WEBINF    = "src/main/webapp/WEB-INF";
 	public static final String WEBINF_TOMCAT = "src/main/webapp-tomcat/WEB-INF";
 	public static final String TEST_RSRC = "src/test/resources";
-	public static final String LIB_DIR   = "target/openolat-lms-13.0-SNAPSHOT/WEB-INF/lib";
+	public static final String LIB_DIR   = "target/openolat-lms-13.1-SNAPSHOT/WEB-INF/lib";
 
 	public static WebArchive createDeployment() {
 		return createDeployment("openolat.war", new HashMap<>());
