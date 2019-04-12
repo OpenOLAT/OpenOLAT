21f2253eace9dbd82bc822f2c8cbc761e42faf4d
uhensler <urs.hensler@frentix.com>
2019-04-12 11:41:27 +0200

    OO-4009: First draft of the OnlyOffice editor (GUI elements)
    Files are not loaded yet.

---
+++ src/main/java/org/olat/core/commons/services/doceditor/onlyoffice/OnlyOfficeEditor.java
@@ -0,0 +1,84 @@
+/**
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
+ * <p>
+ */
+package org.olat.core.commons.services.doceditor.onlyoffice;
+
+import java.util.Locale;
+
+import org.olat.core.commons.services.doceditor.DocEditor;
+import org.olat.core.commons.services.doceditor.DocEditorConfigs;
+import org.olat.core.commons.services.doceditor.DocEditorSecurityCallback;
+import org.olat.core.commons.services.doceditor.onlyoffice.ui.OnlyOfficeEditorController;
+import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.control.Controller;
+import org.olat.core.gui.control.WindowControl;
+import org.olat.core.gui.translator.Translator;
+import org.olat.core.id.Identity;
+import org.olat.core.util.Util;
+import org.olat.core.util.vfs.VFSLeaf;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * Initial date: 11 Apr 2019<br>
+ * @author uhensler, urs.hensler@frentix.com, http://www.frentix.com
+ *
+ */
+@Service
+public class OnlyOfficeEditor implements DocEditor {
+	
+	@Autowired
+	private OnlyOfficeModule onlyOfficeModule;
+
+	@Override
+	public boolean isEnable() {
+		return onlyOfficeModule.isEnabled();
+	}
+
+	@Override
+	public String getType() {
+		return "onlyoffice";
+	}
+
+	@Override
+	public String getDisplayName(Locale locale) {
+		Translator translator = Util.createPackageTranslator(OnlyOfficeEditorController.class, locale);
+		return translator.translate("editor.display.name");
+	}
+
+	@Override
+	public boolean isSupportingFormat(String suffix, Mode mode, boolean hasMeta) {
+		// TODO uh Auto-generated method stub
+		return "docx".equals(suffix);
+	}
+
+	@Override
+	public boolean isLockedForMe(VFSLeaf vfsLeaf, Identity identity, Mode mode) {
+		// TODO uh Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Controller getRunController(UserRequest ureq, WindowControl wControl, Identity identity, VFSLeaf vfsLeaf,
+			DocEditorSecurityCallback securityCallback, DocEditorConfigs configs) {
+		return new OnlyOfficeEditorController(ureq, wControl, vfsLeaf, securityCallback);
+	}
+
+}
