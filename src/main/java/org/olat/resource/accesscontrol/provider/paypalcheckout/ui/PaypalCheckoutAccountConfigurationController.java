f4fab1b3765a2485f7bda95d4ad36690e2a20837
srosse <stephane.rosse@frentix.com>
2019-08-27 07:35:18 +0200

    OO-4203: last missing translations, enhance account settings panel

--- src/main/java/org/olat/resource/accesscontrol/provider/paypalcheckout/ui/PaypalCheckoutAccountConfigurationController.java
+++ src/main/java/org/olat/resource/accesscontrol/provider/paypalcheckout/ui/PaypalCheckoutAccountConfigurationController.java
@@ -22,10 +22,13 @@ package org.olat.resource.accesscontrol.provider.paypalcheckout.ui;
 import java.util.List;
 
 import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.components.form.flexible.FormItem;
 import org.olat.core.gui.components.form.flexible.FormItemContainer;
+import org.olat.core.gui.components.form.flexible.elements.MultipleSelectionElement;
 import org.olat.core.gui.components.form.flexible.elements.SingleSelection;
 import org.olat.core.gui.components.form.flexible.elements.TextElement;
 import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
+import org.olat.core.gui.components.form.flexible.impl.FormEvent;
 import org.olat.core.gui.components.form.flexible.impl.FormLayoutContainer;
 import org.olat.core.gui.components.util.KeyValues;
 import org.olat.core.gui.control.Controller;
@@ -44,9 +47,12 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class PaypalCheckoutAccountConfigurationController extends FormBasicController {
 	
+	private static final String[] onKeys = new String[] { "on" };
+	
 	private TextElement clientIdEl;
 	private TextElement clientSecretEl;
 	private SingleSelection currencyEl;
+	private MultipleSelectionElement enableEl;
 	
 	private final List<String> paypalCurrencies;
 
@@ -59,6 +65,7 @@ public class PaypalCheckoutAccountConfigurationController extends FormBasicContr
 		super(ureq, wControl);
 		paypalCurrencies = paypalModule.getPaypalCurrencies();
 		initForm(ureq);
+		updateUI();
 	}
 	
 	@Override
@@ -69,41 +76,90 @@ public class PaypalCheckoutAccountConfigurationController extends FormBasicContr
 	@Override
 	protected void initForm(FormItemContainer formLayout, Controller listener, UserRequest ureq) {
 		setFormTitle("checkout.config.title");
-
-		if(acModule.isPaypalEnabled()) {
-			setFormDescription("checkout.config.description");
-			setFormContextHelp("PayPal Configuration");
-			
-			KeyValues currencies = new KeyValues();
-			paypalCurrencies.forEach(currency -> currencies.add(KeyValues.entry(currency, currency)));
-			currencyEl = uifactory.addDropdownSingleselect("currency", "currency", formLayout, currencies.keys(), currencies.values(), null);
-			if(StringHelper.containsNonWhitespace(paypalModule.getPaypalCurrency())) {
-				currencyEl.select(paypalModule.getPaypalCurrency(), true);
-			} else {
-				currencyEl.select("CHF", true);
-			}
-			
-			String clientId = paypalModule.getClientId();
-			clientIdEl = uifactory.addTextElement("checkout.client.id", 128, clientId, formLayout);
-			String clientSecret = paypalModule.getClientSecret();
-			clientSecretEl = uifactory.addTextElement("checkout.client.secret", 128, clientSecret, formLayout);
-
-			final FormLayoutContainer buttonGroupLayout = FormLayoutContainer.createButtonLayout("buttonLayout", getTranslator());
-			formLayout.add(buttonGroupLayout);
-			
-			uifactory.addFormSubmitButton("save", buttonGroupLayout);
+		setFormDescription("checkout.config.description");
+		
+		String[] onValues = new String[] { translate("on") };
+		enableEl = uifactory.addCheckboxesHorizontal("enable", "paypal.enable", formLayout, onKeys, onValues);
+		enableEl.select(onKeys[0], acModule.isPaypalCheckoutEnabled());
+		enableEl.addActionListener(FormEvent.ONCHANGE);
+		
+		KeyValues currencies = new KeyValues();
+		paypalCurrencies.forEach(currency -> currencies.add(KeyValues.entry(currency, currency)));
+		currencyEl = uifactory.addDropdownSingleselect("currency", "currency", formLayout, currencies.keys(), currencies.values(), null);
+		if(StringHelper.containsNonWhitespace(paypalModule.getPaypalCurrency())) {
+			currencyEl.select(paypalModule.getPaypalCurrency(), true);
 		} else {
-			String fxSupport = "contact@frentix.com";
-			setFormWarning("config.disabled.warning", new String[]{fxSupport});
+			currencyEl.select("CHF", true);
 		}
+		
+		String clientId = paypalModule.getClientId();
+		clientIdEl = uifactory.addTextElement("checkout.client.id", 128, clientId, formLayout);
+		String clientSecret = paypalModule.getClientSecret();
+		clientSecretEl = uifactory.addTextElement("checkout.client.secret", 128, clientSecret, formLayout);
+
+		final FormLayoutContainer buttonGroupLayout = FormLayoutContainer.createButtonLayout("buttonLayout", getTranslator());
+		formLayout.add(buttonGroupLayout);
+		
+		uifactory.addFormSubmitButton("save", buttonGroupLayout);
+	}
+	
+	private void updateUI() {
+		boolean enabled = enableEl.isAtLeastSelected(1);
+		currencyEl.setVisible(enabled);
+		clientIdEl.setVisible(enabled);
+		clientSecretEl.setVisible(enabled);
+	}
+
+	@Override
+	protected boolean validateFormLogic(UserRequest ureq) {
+		boolean allOk = super.validateFormLogic(ureq);
+
+		currencyEl.clearError();
+		if(enableEl.isAtLeastSelected(1) && !currencyEl.isOneSelected()) {
+			currencyEl.setErrorKey("form.legende.mandatory", null);
+			allOk &= false;
+		}
+		
+		allOk &= validateId(clientIdEl);
+		allOk &= validateId(clientSecretEl);
+		return allOk;
+	}
+	
+	private boolean validateId(TextElement element) {
+		boolean allOk = true;
+		
+		element.clearError();
+		if(enableEl.isAtLeastSelected(1)
+				&& !StringHelper.containsNonWhitespace(element.getValue())) {
+			element.setErrorKey("form.legende.mandatory", null);
+			allOk &= false;
+			
+		}
+		
+		return allOk;
+	}
+
+	@Override
+	protected void formInnerEvent(UserRequest ureq, FormItem source, FormEvent event) {
+		if(source == enableEl) {
+			updateUI();
+		}
+		super.formInnerEvent(ureq, source, event);
 	}
 
 	@Override
 	protected void formOK(UserRequest ureq) {
-		paypalModule.setClientId(clientIdEl.getValue());
-		paypalModule.setClientSecret(clientSecretEl.getValue());
-		if(currencyEl.isOneSelected() && paypalCurrencies.contains(currencyEl.getSelectedKey())) {
-			paypalModule.setPaypalCurrency(currencyEl.getSelectedKey());
+		boolean enabled = enableEl.isAtLeastSelected(1);
+		acModule.setPaypalCheckoutEnabled(enabled);
+		if(enabled) {
+			paypalModule.setClientId(clientIdEl.getValue());
+			paypalModule.setClientSecret(clientSecretEl.getValue());
+			if(currencyEl.isOneSelected() && paypalCurrencies.contains(currencyEl.getSelectedKey())) {
+				paypalModule.setPaypalCurrency(currencyEl.getSelectedKey());
+			}
+		} else {
+			paypalModule.setClientId(null);
+			paypalModule.setClientSecret(null);
 		}
 		showInfo("saved");
 	}
