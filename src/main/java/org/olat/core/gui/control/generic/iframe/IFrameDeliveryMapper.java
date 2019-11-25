532ae4fcf85444519c4287ee97599978b9d2bb23
srosse <stephane.rosse@frentix.com>
2019-11-25 21:12:56 +0100

    OO-4220: update jQuery to 3.4.1

--- src/main/java/org/olat/core/gui/control/generic/iframe/IFrameDeliveryMapper.java
+++ src/main/java/org/olat/core/gui/control/generic/iframe/IFrameDeliveryMapper.java
@@ -565,7 +565,7 @@ public class IFrameDeliveryMapper implements Mapper {
 		}
 		
 		public void appendJQuery2Cond() {
-			appendStaticJs("js/jquery/jquery-3.3.1.min.js");
+			appendStaticJs("js/jquery/jquery-3.4.1.min.js");
 		}
 
 		public void appendPrototype() {
