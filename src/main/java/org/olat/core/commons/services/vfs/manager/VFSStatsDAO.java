0ed405b86cb207db89fb7c24d4e8135fb205d875
aboeckle <alexander.boeckle@frentix.com>
2019-12-23 13:27:25 +0100

    OO-4375 Added license and author

--- src/main/java/org/olat/core/commons/services/vfs/manager/VFSStatsDAO.java
+++ src/main/java/org/olat/core/commons/services/vfs/manager/VFSStatsDAO.java
@@ -1,3 +1,22 @@
+/**
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
+ * <p>
+ */
 package org.olat.core.commons.services.vfs.manager;
 
 import java.util.List;
@@ -6,6 +25,12 @@ import org.olat.core.commons.persistence.DB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 
+ * Initial date: 23 Dec 2019<br>
+ * @author aboeckle, alexander.boeckle@frentix.com, http://www.frentix.com
+ *
+ */
 @Service
 public class VFSStatsDAO {
 
