b08cb6fe7b0d306490007f306933be56200a798c
srosse <stephane.rosse@frentix.com>
2020-03-25 18:48:28 +0100

    OO-4584: add an secret option about breaout rooms in BBB

--- src/main/resources/database/postgresql/setupDatabase.sql
+++ src/main/resources/database/postgresql/setupDatabase.sql
@@ -1256,6 +1256,7 @@ create table o_bbb_template (
    b_max_participants int8 default null,
    b_max_duration int8 default null,
    b_record bool default null,
+   b_breakout bool default null,
    b_mute_on_start bool default null,
    b_auto_start_recording bool default null,
    b_allow_start_stop_recording bool default null,
