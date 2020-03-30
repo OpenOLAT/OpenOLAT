e0d702be19c8df6467822cda3746534bbc7589a8
srosse <stephane.rosse@frentix.com>
2020-03-30 07:23:21 +0200

    OO-4601: check if forum to delete is available

--- src/main/java/org/olat/modules/fo/manager/ForumManager.java
+++ src/main/java/org/olat/modules/fo/manager/ForumManager.java
@@ -40,6 +40,7 @@ import java.util.Set;
 import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 
+import org.apache.logging.log4j.Logger;
 import org.olat.basesecurity.IdentityRef;
 import org.olat.core.commons.modules.bc.FolderConfig;
 import org.olat.core.commons.persistence.DB;
@@ -49,8 +50,6 @@ import org.olat.core.commons.services.mark.impl.MarkImpl;
 import org.olat.core.commons.services.text.TextService;
 import org.olat.core.id.Identity;
 import org.olat.core.id.OLATResourceable;
-import org.olat.core.logging.AssertException;
-import org.apache.logging.log4j.Logger;
 import org.olat.core.logging.Tracing;
 import org.olat.core.util.Encoder;
 import org.olat.core.util.Encoder.Algorithm;
@@ -730,11 +729,12 @@ public class ForumManager {
 	 */
 	public void deleteForum(Long forumKey) {
 		Forum foToDel = loadForum(forumKey);
-		if (foToDel == null) throw new AssertException("forum to delete was not found: key=" + forumKey);
-		// delete properties, messages and the forum itself
-		doDeleteForum(foToDel);
-		// delete directory for messages with attachments
-		deleteForumContainer(forumKey);
+		if (foToDel != null) {
+			// delete properties, messages and the forum itself
+			doDeleteForum(foToDel);
+			// delete directory for messages with attachments
+			deleteForumContainer(forumKey);
+		}
 	}
 
 	/**
@@ -990,7 +990,7 @@ public class ForumManager {
 		} else if(forumContainer instanceof VFSContainer) {
 			return (VFSContainer)forumContainer;
 		}
-		log.error("The following forum container is not a directory: " + forumContainer);
+		log.error("The following forum container is not a directory: {}", forumContainer);
 		return null;
 	}
 	
