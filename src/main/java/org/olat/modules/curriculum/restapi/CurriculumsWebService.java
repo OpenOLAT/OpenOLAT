1e005e342586f3abd01baf79b90d9120adab8d2b
Moritzjenny <moritzjenny@hotmail.com>
2020-07-16 12:07:31 +0200

    no-jira: fix swagger annotation import

--- src/main/java/org/olat/modules/curriculum/restapi/CurriculumsWebService.java
+++ src/main/java/org/olat/modules/curriculum/restapi/CurriculumsWebService.java
@@ -67,6 +67,7 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
--- src/main/java/org/olat/restapi/system/ThreadsWebService.java
+++ src/main/java/org/olat/restapi/system/ThreadsWebService.java
@@ -43,6 +43,7 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 /**
  * 
