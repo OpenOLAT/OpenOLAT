b729af6c1e1c1ad4f41794ca41ee32aeb7844229
srosse <stephane.rosse@frentix.com>
2020-10-26 11:37:35 +0100

    OO-5000: scroll top after selecting a folder in briefcase

--- src/main/java/org/olat/core/commons/modules/bc/FolderRunController.java
+++ src/main/java/org/olat/core/commons/modules/bc/FolderRunController.java
@@ -60,6 +60,7 @@ import org.olat.core.gui.control.WindowControl;
 import org.olat.core.gui.control.controller.BasicController;
 import org.olat.core.gui.control.generic.closablewrapper.CloseableModalController;
 import org.olat.core.gui.control.generic.dtabs.Activateable2;
+import org.olat.core.gui.control.winmgr.ScrollTopCommand;
 import org.olat.core.id.OLATResourceable;
 import org.olat.core.id.Roles;
 import org.olat.core.id.context.BusinessControl;
@@ -486,6 +487,7 @@ public class FolderRunController extends BasicController implements Activateable
 			
 			if(FolderCommandFactory.COMMAND_BROWSE.equals(cmd)) {
 				updatePathResource(ureq);
+				getWindowControl().getWindowBackOffice().sendCommandTo(new ScrollTopCommand());
 			}
 			enableDisableQuota(ureq);
 		}
