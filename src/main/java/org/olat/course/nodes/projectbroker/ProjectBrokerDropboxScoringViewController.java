d4673cf56dadeefe886292631bcd4941bc9284ae
srosse <stephane.rosse@frentix.com>
2020-11-19 18:48:59 +0100

    OO-5089: put real names on the topic assignments folders

--- src/main/java/org/olat/course/nodes/projectbroker/ProjectBrokerDropboxScoringViewController.java
+++ src/main/java/org/olat/course/nodes/projectbroker/ProjectBrokerDropboxScoringViewController.java
@@ -26,14 +26,24 @@
 package org.olat.course.nodes.projectbroker;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.olat.core.CoreSpringFactory;
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.control.WindowControl;
 import org.olat.core.gui.translator.Translator;
+import org.olat.core.id.Identity;
+import org.olat.core.util.StringHelper;
 import org.olat.core.util.Util;
+import org.olat.core.util.vfs.MergeSource;
+import org.olat.core.util.vfs.NamedContainerImpl;
+import org.olat.core.util.vfs.VFSContainer;
+import org.olat.core.util.vfs.VFSItem;
+import org.olat.core.util.vfs.VFSManager;
 import org.olat.core.util.vfs.callbacks.ReadOnlyCallback;
 import org.olat.core.util.vfs.callbacks.VFSSecurityCallback;
+import org.olat.core.util.vfs.filters.VFSContainerFilter;
 import org.olat.course.nodes.CourseNode;
 import org.olat.course.nodes.projectbroker.datamodel.Project;
 import org.olat.course.nodes.projectbroker.service.ProjectGroupManager;
@@ -63,28 +73,68 @@ public class ProjectBrokerDropboxScoringViewController extends DropboxScoringVie
 		super(ureq, wControl, node, userCourseEnv, false);	
 		this.project = project;
 		projectGroupManager = CoreSpringFactory.getImpl(ProjectGroupManager.class);
-		this.setVelocityRoot(Util.getPackageVelocityRoot(DropboxScoringViewController.class));
-		Translator fallbackTranslator = Util.createPackageTranslator(this.getClass(), ureq.getLocale());
-		Translator myTranslator = Util.createPackageTranslator(DropboxScoringViewController.class, ureq.getLocale(), fallbackTranslator);
+		setVelocityRoot(Util.getPackageVelocityRoot(DropboxScoringViewController.class));
+		Translator fallbackTranslator = Util.createPackageTranslator(this.getClass(), getLocale());
+		Translator myTranslator = Util.createPackageTranslator(DropboxScoringViewController.class, getLocale(), fallbackTranslator);
 		setTranslator(myTranslator);
 		boolean hasNotification = projectGroupManager.isProjectManagerOrAdministrator(ureq, userCourseEnv.getCourseEnvironment(), project);
 		init(ureq, hasNotification);
 	}
 	
 	@Override
-	protected String getDropboxFilePath(String assesseeName) {
-		return DropboxController.getDropboxPathRelToFolderRoot(userCourseEnv.getCourseEnvironment(), node)
-		+ File.separator + project.getKey();
+	protected VFSContainer getDropboxFilePath(Identity assesseeIdentity) {
+		String path = DropboxController.getDropboxPathRelToFolderRoot(userCourseEnv.getCourseEnvironment(), node)
+				+ File.separator + project.getKey();
+		VFSContainer rootContainer = VFSManager.olatRootContainer(path, null);
+		List<VFSItem> items = rootContainer.getItems(new VFSContainerFilter());
+		List<VFSContainer> namedContainers = new ArrayList<>(items.size());
+		VFSSecurityCallback readOnly = getDropboxVfsSecurityCallback();
+		for(VFSItem item: items) {
+			if(item instanceof VFSContainer) {
+				String name = StringHelper.escapeHtml(userManager.getUserDisplayName(item.getName()));
+				NamedContainerImpl named = new NamedContainerImpl(name, (VFSContainer)item);
+				named.setLocalSecurityCallback(readOnly);
+				namedContainers.add(named);
+			}
+		}
+		
+		MergeSource container = new BoxSource(rootContainer, translate("dropbox.title"), namedContainers);
+		container.setLocalSecurityCallback(readOnly);
+		return container;
 	}
 
 	@Override
-	protected String getReturnboxFilePath(String assesseeName) {
-		return ReturnboxController.getReturnboxPathRelToFolderRoot(userCourseEnv.getCourseEnvironment(), node) 
-		+ File.separator + project.getKey();
+	protected VFSContainer getReturnboxFilePath(Identity assesseeIdentity) {
+		String path = ReturnboxController.getReturnboxPathRelToFolderRoot(userCourseEnv.getCourseEnvironment(), node) 
+				+ File.separator + project.getKey();
+		VFSContainer rootContainer = VFSManager.olatRootContainer(path, null);
+		List<VFSItem> items = rootContainer.getItems(new VFSContainerFilter());
+		List<VFSContainer> namedContainers = new ArrayList<>(items.size());
+		VFSSecurityCallback secCallback = getReturnboxVfsSecurityCallback(path, assesseeIdentity);
+		for(VFSItem item: items) {
+			if(item instanceof VFSContainer) {
+				String name = StringHelper.escapeHtml(userManager.getUserDisplayName(item.getName()));
+				NamedContainerImpl named = new NamedContainerImpl(name, (VFSContainer)item);
+				named.setLocalSecurityCallback(secCallback);
+				namedContainers.add(named);
+			}
+		}
+		
+		MergeSource container = new BoxSource(rootContainer, translate("returnbox.title"), namedContainers);
+		container.setLocalSecurityCallback(secCallback);
+		return container;
 	}
 
 	@Override
 	protected VFSSecurityCallback getDropboxVfsSecurityCallback() {
 		return new ReadOnlyCallback();
+	}
+	
+	private static class BoxSource extends MergeSource {
+		
+		public BoxSource(VFSContainer rootContainer, String name, List<VFSContainer> containers) {
+			super(rootContainer, name);
+			setMergedContainers(containers);
+		}
 	}
 }
