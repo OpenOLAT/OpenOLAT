cb5bd3ff82e50c1cad75bf25cbf4f9255ef1a67e
srosse <stephane.rosse@frentix.com>
2021-01-15 10:27:53 +0100

    OO-5228: login date as precedence over reactivation date

--- src/main/java/org/olat/user/manager/lifecycle/UserLifecycleManagerImpl.java
+++ src/main/java/org/olat/user/manager/lifecycle/UserLifecycleManagerImpl.java
@@ -98,17 +98,22 @@ public class UserLifecycleManagerImpl implements UserLifecycleManager {
 	
 	@Override
 	public long getDaysUntilDeactivation(IdentityLifecycle identity, Date referenceDate) {
-		long days;
+		Date lastLogin = identity.getLastLogin();
+		if(lastLogin == null) {
+			lastLogin = identity.getCreationDate();
+		}
+		long days = userModule.getNumberOfInactiveDayBeforeDeactivation() - CalendarUtils.numOfDays(referenceDate, lastLogin);
+		if(days < 0l) {
+			days = 0l;
+		}
 		Date reactivationDate = identity.getReactivationDate();
 		if(reactivationDate != null ) {
-			days = userModule.getNumberOfDayReactivationPeriod() - CalendarUtils.numOfDays(referenceDate, reactivationDate);
-		} else {
-			Date lastLogin = identity.getLastLogin();
-			if(lastLogin == null) {
-				lastLogin = identity.getCreationDate();
+			long reactivationDays = userModule.getNumberOfDayReactivationPeriod() - CalendarUtils.numOfDays(referenceDate, reactivationDate);
+			if(reactivationDays > days) {
+				days = reactivationDays;
 			}
-			days = userModule.getNumberOfInactiveDayBeforeDeactivation() - CalendarUtils.numOfDays(referenceDate, lastLogin);
 		}
+		
 		return days > 0l ? days : 1l;
 	}
 
