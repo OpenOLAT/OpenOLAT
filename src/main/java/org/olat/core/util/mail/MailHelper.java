47a5b19159e15a0cc2c9a270e547b27cf5d4a60a
aboeckle <alexander.boeckle@frentix.com>
2021-07-23 17:33:25 +0200

    OO-5605: Fix Mail Regex in Footer

--- src/main/java/org/olat/core/util/mail/MailHelper.java
+++ src/main/java/org/olat/core/util/mail/MailHelper.java
@@ -63,8 +63,9 @@ import org.olat.user.propertyhandlers.UserPropertyHandler;
  */
 public class MailHelper {
 	
-	private static final Comparator<String> CASE_INSENSTIVE = Comparator.comparing(Function.identity(),
-			String.CASE_INSENSITIVE_ORDER);
+	public static final String MAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+	
+	private static final Comparator<String> CASE_INSENSTIVE = Comparator.comparing(Function.identity(), String.CASE_INSENSITIVE_ORDER);
 	
 	private static Map<String, Translator> translators = new HashMap<>();
 	
