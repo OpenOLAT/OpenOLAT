ffd9d62f14878b190a34c33225ead98f36b80303
uhensler <urs.hensler@frentix.com>
2021-09-03 07:43:07 +0200

    OO-5685: Reduce information density in tab "Title and description"

--- src/main/java/org/olat/course/editor/NodeConfigController.java
+++ src/main/java/org/olat/course/editor/NodeConfigController.java
@@ -21,12 +21,16 @@
 package org.olat.course.editor;
 
 import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.components.form.flexible.FormItem;
 import org.olat.core.gui.components.form.flexible.FormItemContainer;
+import org.olat.core.gui.components.form.flexible.elements.FormLink;
 import org.olat.core.gui.components.form.flexible.elements.RichTextElement;
 import org.olat.core.gui.components.form.flexible.elements.TextElement;
 import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
+import org.olat.core.gui.components.form.flexible.impl.FormEvent;
 import org.olat.core.gui.components.form.flexible.impl.FormLayoutContainer;
 import org.olat.core.gui.components.form.flexible.impl.elements.richText.TextMode;
+import org.olat.core.gui.components.link.Link;
 import org.olat.core.gui.control.Controller;
 import org.olat.core.gui.control.WindowControl;
 import org.olat.core.util.Formatter;
@@ -49,20 +53,27 @@ public class NodeConfigController extends FormBasicController {
 	private TextElement shortTitleEl;
 	private TextElement titleEl;
 	private RichTextElement descriptionEl;
+	private FormLink showAdditionalLink;
 	private RichTextElement objectivesEl;
 	private RichTextElement instructionEl;
 	private RichTextElement instructionalDesignEl;
 
 	private final CourseNode courseNode;
 	private final UserCourseEnvironment userCourseEnv;
+	private boolean showAdditional;
+	
 
 	public NodeConfigController(UserRequest ureq, WindowControl wControl, CourseNode courseNode,
 			UserCourseEnvironment userCourseEnv) {
 		super(ureq, wControl);
 		this.courseNode = courseNode;
 		this.userCourseEnv = userCourseEnv;
+		this.showAdditional = StringHelper.containsNonWhitespace(courseNode.getObjectives())
+				|| StringHelper.containsNonWhitespace(courseNode.getInstruction())
+				|| StringHelper.containsNonWhitespace(courseNode.getInstructionalDesign());
 
 		initForm(ureq);
+		updateUI();
 	}
 
 	@Override
@@ -91,6 +102,9 @@ public class NodeConfigController extends FormBasicController {
 				new LazyRepositoryEdusharingProvider(
 						userCourseEnv.getCourseEditorEnv().getCourseGroupManager().getCourseEntry().getKey(),
 						"course-learning-objectives-" + courseNode.getIdent()));
+		
+		showAdditionalLink = uifactory.addFormLink("show.additional", "nodeConfigForm.show.additional", null, formLayout, Link.LINK);
+		showAdditionalLink.setIconLeftCSS("o_icon o_icon-lg o_icon_open_togglebox");
 
 		objectivesEl = uifactory.addRichTextElementForStringData("nodeConfigForm.objectives",
 				"nodeConfigForm.objectives", courseNode.getObjectives(), 10, -1, false, null, null, formLayout,
@@ -129,6 +143,22 @@ public class NodeConfigController extends FormBasicController {
 		formLayout.add(buttonLayout);
 		uifactory.addFormSubmitButton("nodeConfigForm.save", buttonLayout)
 				.setElementCssClass("o_sel_node_editor_submit");
+	}
+
+	private void updateUI() {
+		showAdditionalLink.setVisible(!showAdditional);
+		objectivesEl.setVisible(showAdditional);
+		instructionEl.setVisible(showAdditional);
+		instructionalDesignEl.setVisible(showAdditional);
+	}
+
+	@Override
+	protected void formInnerEvent(UserRequest ureq, FormItem source, FormEvent event) {
+		if (source == showAdditionalLink) {
+			showAdditional = true;
+			updateUI();
+		}
+		super.formInnerEvent(ureq, source, event);
 	}
 
 	@Override
