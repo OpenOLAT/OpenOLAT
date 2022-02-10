1bb4ee044e839908521925fe527936657844cb15
srosse <stephane.rosse@frentix.com>
2022-02-10 13:49:29 +0100

    OO-6013: use compensation disadvantage to calculate end window of test

--- src/main/java/org/olat/core/util/DateUtils.java
+++ src/main/java/org/olat/core/util/DateUtils.java
@@ -150,6 +150,13 @@ public class DateUtils {
 		return c.getTime();
 	}
 	
+	public static Date addSeconds(Date date, int seconds) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		c.add(Calendar.SECOND, seconds);
+		return c.getTime();
+	}
+	
 	public static Date getLater(Date date1, Date date2) {
 		if (date1 == null) return date2;
 		if (date2 == null) return date1;
