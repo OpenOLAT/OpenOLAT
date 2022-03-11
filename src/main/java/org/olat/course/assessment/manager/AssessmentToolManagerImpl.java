1a24fb3da35918ecf86b3e5c622cc2cf8cde5c11
srosse <stephane.rosse@frentix.com>
2022-03-11 09:36:06 +0100

    OO-6061: add filter for text based user properties in assessment tool

--- src/main/java/org/olat/course/assessment/manager/AssessmentToolManagerImpl.java
+++ src/main/java/org/olat/course/assessment/manager/AssessmentToolManagerImpl.java
@@ -475,6 +475,12 @@ public class AssessmentToolManagerImpl implements AssessmentToolManager {
 		if(params.getAssessmentObligations() != null && !params.getAssessmentObligations().isEmpty()) {
 			list.setParameter("assessmentObligations", params.getAssessmentObligations());
 		}
+		if(params.getUserProperties() != null && !params.getUserProperties().isEmpty()) {
+			for(Map.Entry<String, String> entry:params.getUserProperties().entrySet()) {
+				String fuzzyValue = PersistenceHelper.makeFuzzyQueryString(entry.getValue());
+				list.setParameter("uprop" + entry.getKey(), fuzzyValue);
+			}
+		}
 	}
 	
 	private void applySearchAssessedIdentityParams(QueryBuilder sb, SearchAssessedIdentityParams params, AssessmentEntryStatus status) {
@@ -526,6 +532,13 @@ public class AssessmentToolManagerImpl implements AssessmentToolManager {
 	          .append("  )");
 		}
 		sb.append(" )");
+		
+		if(params.getUserProperties() != null && !params.getUserProperties().isEmpty()) {
+			for(Map.Entry<String, String> entry:params.getUserProperties().entrySet()) {
+				sb.append(" and ")
+				  .appendFuzzyLike("assessedUser." + entry.getKey(), "uprop" + entry.getKey());
+			}
+		}
 	}
 
 	@Override
