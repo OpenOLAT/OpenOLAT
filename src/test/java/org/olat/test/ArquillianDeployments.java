ac4cf1c4ab15c641f883fdf62fb99b57c4c0b27e
srosse <stephane.rosse@frentix.com>
2022-04-01 16:28:45 +0200

    no-jira: prepare for 17.0 alpha

--- src/test/java/org/olat/test/ArquillianDeployments.java
+++ src/test/java/org/olat/test/ArquillianDeployments.java
@@ -47,7 +47,7 @@ public class ArquillianDeployments {
 	public static final String WEBINF    = "src/main/webapp/WEB-INF";
 	public static final String WEBINF_TOMCAT = "src/main/webapp-tomcat/WEB-INF";
 	public static final String TEST_RSRC = "src/test/resources";
-	public static final String LIB_DIR   = "target/openolat-lms-16.2-SNAPSHOT/WEB-INF/lib";
+	public static final String LIB_DIR   = "target/openolat-lms-17.0-SNAPSHOT/WEB-INF/lib";
 
 	public static WebArchive createDeployment() {
 		Map<String,String> overrideProperties = new HashMap<>();
