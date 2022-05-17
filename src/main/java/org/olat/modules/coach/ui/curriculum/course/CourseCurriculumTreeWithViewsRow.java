a75508364c85fe5df4942ad4edc6713f141bc735
srosse <stephane.rosse@frentix.com>
2022-05-17 18:46:12 +0200

    no-jira: fix rs if row has no entry status

--- src/main/java/org/olat/modules/coach/ui/curriculum/course/CourseCurriculumTreeWithViewsRow.java
+++ src/main/java/org/olat/modules/coach/ui/curriculum/course/CourseCurriculumTreeWithViewsRow.java
@@ -368,7 +368,7 @@ public class CourseCurriculumTreeWithViewsRow implements CurriculumElementWithVi
 	}
 	
 	public boolean isClosed() {
-		return status.decommissioned();
+		return status != null && status.decommissioned();
 	}
 
 	public boolean isSingleEntry() {
