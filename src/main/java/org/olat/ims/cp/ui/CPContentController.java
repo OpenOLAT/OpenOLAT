bce2f8eaf6f5a779b7fec73f07cdd2f2dac26dc8
Christoph Pfranger <christoph.pfranger@frentix.com>
2022-06-15 09:41:25 +0200

    OO-6263: update context help

--- src/main/java/org/olat/ims/cp/ui/CPContentController.java
+++ src/main/java/org/olat/ims/cp/ui/CPContentController.java
@@ -90,7 +90,8 @@ public class CPContentController extends BasicController {
 		// init help link, can't do this in initToolbar because ureq is missing
 		if (helpModule.isHelpEnabled()) {
 			HelpLinkSPI provider = helpModule.getManualProvider();
-			helpLink = provider.getHelpPageLink(ureq, translate("help"), translate("helpbutton"), "o_icon o_icon-lg o_icon_help", null, "CP Editor");
+			helpLink = provider.getHelpPageLink(ureq, translate("help"), translate("helpbutton"),
+					"o_icon o_icon-lg o_icon_help", null, "manual_user/resource_cp/CP_Editor/");
 		}
 		
 		// set initial page to display
