d6e2f993bec454e7ad0c62e464f195f6c76b2045
uhensler <urs.hensler@frentix.com>
2022-07-04 16:24:23 +0200

    OO-6143: Better link presentation in share tab

--- src/main/java/org/olat/repository/ui/author/AuthoringEditAccessShareController.java
+++ src/main/java/org/olat/repository/ui/author/AuthoringEditAccessShareController.java
@@ -34,6 +34,7 @@ import org.olat.basesecurity.model.OrganisationRefImpl;
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.components.form.flexible.FormItem;
 import org.olat.core.gui.components.form.flexible.FormItemContainer;
+import org.olat.core.gui.components.form.flexible.elements.FormLink;
 import org.olat.core.gui.components.form.flexible.elements.MultipleSelectionElement;
 import org.olat.core.gui.components.form.flexible.elements.SelectionElement;
 import org.olat.core.gui.components.form.flexible.elements.SingleSelection;
@@ -41,6 +42,7 @@ import org.olat.core.gui.components.form.flexible.impl.Form;
 import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
 import org.olat.core.gui.components.form.flexible.impl.FormEvent;
 import org.olat.core.gui.components.form.flexible.impl.FormLayoutContainer;
+import org.olat.core.gui.components.link.Link;
 import org.olat.core.gui.components.util.SelectionValues;
 import org.olat.core.gui.control.Controller;
 import org.olat.core.gui.control.Event;
@@ -86,7 +88,9 @@ public class AuthoringEditAccessShareController extends FormBasicController {
 	private static final String[] accessKey = new String[] { KEY_PRIVATE, KEY_PUBLIC };
 	
 	private SingleSelection accessEl;
+	private FormLayoutContainer repoLinkCont;
 	private FormLayoutContainer catalogLinksCont;
+	private FormLink showMicrositeLinks;
 	private SingleSelection leaveEl;
 	private SingleSelection statusEl;
 	private MultipleSelectionElement organisationsEl;
@@ -112,7 +116,6 @@ public class AuthoringEditAccessShareController extends FormBasicController {
 	private CatalogV2Module catalogModule;
 	@Autowired
 	private TaxonomyModule taxonomyModule;
-	
 	public AuthoringEditAccessShareController(UserRequest ureq, WindowControl wControl, RepositoryEntry entry, boolean readOnly) {
 		super(ureq, wControl);
 		setTranslator(Util.createPackageTranslator(TaxonomyUIFactory.class, getLocale(), getTranslator()));
@@ -227,10 +230,21 @@ public class AuthoringEditAccessShareController extends FormBasicController {
 			accessEl.select(KEY_PRIVATE, true);
 		}
 		
+		repoLinkCont = FormLayoutContainer.createCustomFormLayout("catalogLinks", getTranslator(), velocity_root + "/repo_links.html");
+		repoLinkCont.setLabel("cif.repo.link", null);
+		repoLinkCont.setRootForm(mainForm);
+		formLayout.add("repoLink", repoLinkCont);
+		String url = Settings.getServerContextPathURI() + "/url/RepositoryEntry/0/" + entry.getKey();
+		repoLinkCont.contextPut("repoLink", new ExtLink(entry.getKey().toString(), url, null));
+		
 		catalogLinksCont = FormLayoutContainer.createCustomFormLayout("catalogLinks", getTranslator(), velocity_root + "/catalog_links.html");
 		catalogLinksCont.setLabel("cif.catalog.links", null);
 		catalogLinksCont.setRootForm(mainForm);
 		formLayout.add("catalogLinks", catalogLinksCont);
+		
+		showMicrositeLinks = uifactory.addFormLink("show.additional", "nodeConfigForm.show.additional", null, catalogLinksCont, Link.LINK);
+		showMicrositeLinks.setIconLeftCSS("o_icon o_icon-lg o_icon_open_togglebox");
+		
 		updateCatalogLinksUI();
 		
 		initLeaveOption(formLayout);
@@ -396,21 +410,26 @@ public class AuthoringEditAccessShareController extends FormBasicController {
 
 	private void updateCatalogLinksUI() {
 		catalogLinksCont.setVisible(catalogModule.isEnabled() && accessEl.isKeySelected(KEY_PUBLIC));
+		
 		if (catalogLinksCont.isVisible()) {
 			String url = Settings.getServerContextPathURI() + "/url/Catalog/0/" + CatalogMainController.ORES_TYPE_SEARCH
 					+ "/0/" + CatalogMainController.ORES_TYPE_INFOS + "/" + entry.getKey();
 			catalogLinksCont.contextPut("searchLink", new ExtLink(entry.getKey().toString(), url, null));
 			
+			showMicrositeLinks.setVisible(false);
 			if (taxonomyModule.isEnabled()) {
 				HashSet<TaxonomyLevel> taxonomyLevels = new HashSet<>(repositoryService.getTaxonomy(entry));
-				List<ExtLink> taxonomyLinks = new ArrayList<>(taxonomyLevels.size());
-				for (TaxonomyLevel taxonomyLevel : taxonomyLevels) {
-					url = Settings.getServerContextPathURI() + "/url/Catalog/0/" + CatalogMainController.ORES_TYPE_TAXONOMY
-							+ "/" + taxonomyLevel.getKey();
-					String name = translate("cif.catalog.links.microsite", TaxonomyUIFactory.translateDisplayName(getTranslator(), taxonomyLevel));
-					ExtLink extLink = new ExtLink(taxonomyLevel.getKey().toString(), url, name);
-					taxonomyLinks.add(extLink);
-					catalogLinksCont.contextPut("taxonomyLinks", taxonomyLinks);
+				if (!taxonomyLevels.isEmpty()) {
+					showMicrositeLinks.setVisible(true);
+					List<ExtLink> taxonomyLinks = new ArrayList<>(taxonomyLevels.size());
+					for (TaxonomyLevel taxonomyLevel : taxonomyLevels) {
+						url = Settings.getServerContextPathURI() + "/url/Catalog/0/" + CatalogMainController.ORES_TYPE_TAXONOMY
+								+ "/" + taxonomyLevel.getKey();
+						String name = translate("cif.catalog.links.microsite", TaxonomyUIFactory.translateDisplayName(getTranslator(), taxonomyLevel));
+						ExtLink extLink = new ExtLink(taxonomyLevel.getKey().toString(), url, name);
+						taxonomyLinks.add(extLink);
+						catalogLinksCont.contextPut("taxonomyLinks", taxonomyLinks);
+					}
 				}
 			}
 		}
