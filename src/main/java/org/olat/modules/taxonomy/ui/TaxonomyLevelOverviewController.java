9d7b51a6441751a6b2650ec7ca5a3b6318450cf3
uhensler <urs.hensler@frentix.com>
2022-07-26 08:44:02 +0200

    OO-6319: RS when switching tabs in taxonomy level editor

--- src/main/java/org/olat/modules/taxonomy/ui/TaxonomyLevelOverviewController.java
+++ src/main/java/org/olat/modules/taxonomy/ui/TaxonomyLevelOverviewController.java
@@ -90,7 +90,7 @@ public class TaxonomyLevelOverviewController extends BasicController implements 
 		
 		tabPane.addTab(ureq, translate("taxonomy.metadata"), uureq -> {
 			removeAsListenerAndDispose(metadataCtrl);
-			metadataCtrl = new EditTaxonomyLevelController(ureq, getWindowControl(), taxonomyLevel);
+			metadataCtrl = new EditTaxonomyLevelController(uureq, getWindowControl(), taxonomyLevel);
 			listenTo(metadataCtrl);
 			return metadataCtrl.getInitialComponent();
 		}, true);
