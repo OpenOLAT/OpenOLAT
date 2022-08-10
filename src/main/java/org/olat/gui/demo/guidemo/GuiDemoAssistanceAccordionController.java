7a0cf2a61aba614a46665e081de198ae022a2b0a
cpfranger <christoph.pfranger@frentix.com>
2022-08-10 09:13:27 +0200

    OO-6332: Add license headers

--- src/main/java/org/olat/gui/demo/guidemo/GuiDemoAssistanceAccordionController.java
+++ src/main/java/org/olat/gui/demo/guidemo/GuiDemoAssistanceAccordionController.java
@@ -1,3 +1,22 @@
+/**
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
+ * <p>
+ */
 package org.olat.gui.demo.guidemo;
 
 import org.olat.core.commons.controllers.accordion.AssistanceAccordionController;
@@ -10,6 +29,12 @@ import org.olat.core.gui.control.Event;
 import org.olat.core.gui.control.WindowControl;
 import org.olat.core.gui.control.controller.BasicController;
 
+/**
+ *
+ * Initial date: 2022-08-10<br>
+ * @author cpfranger, christoph.pfranger@frentix.com, https://www.frentix.com
+ *
+ */
 public class GuiDemoAssistanceAccordionController extends BasicController {
 
     private final Link button1;
