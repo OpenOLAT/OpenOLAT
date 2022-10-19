11d223d7939875c7fffb1409d8295ad5f72a2981
srosse <stephane.rosse@frentix.com>
2022-10-19 07:28:30 +0200

    OO-6521: reduce number of in the sub-select

--- src/main/java/org/olat/repository/manager/RepositoryEntryMyCourseQueries.java
+++ src/main/java/org/olat/repository/manager/RepositoryEntryMyCourseQueries.java
@@ -489,7 +489,8 @@ public class RepositoryEntryMyCourseQueries {
 			//make sure that in all case the role is mandatory
 			sb.append(" v.key in (select re4.key from repositoryentry as re4 ")
 			  .append("   inner join repoentrytogroup as rel4 on (re4.key=rel4.entry.key)")
-			  .append("   inner join bgroupmember as membership on (membership.group.key=rel4.group.key)")
+			  .append("   inner join rel4.group as bsgrp")
+			  .append("   inner join bsgrp.members as membership")
 			  .append("   where membership.identity.key=:identityKey and (")
 			  .append("    membership.role").in(inRoles.stream().toArray(GroupRoles[]::new))
 			  .append("    and re4.status ").in(memberShipsStatus)
