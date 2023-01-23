0786d7c408ee826fead87b44bbacadb6d927369c
uhensler <urs.hensler@frentix.com>
2023-01-23 12:24:37 +0100

    OO-6719: Deliver images twice as large for retina displays

--- src/main/java/org/olat/modules/catalog/ui/CatalogLauncherRepositoryEntriesController.java
+++ src/main/java/org/olat/modules/catalog/ui/CatalogLauncherRepositoryEntriesController.java
@@ -91,7 +91,8 @@ public class CatalogLauncherRepositoryEntriesController extends BasicController 
 		this.state = state;
 		setTranslator(Util.createPackageTranslator(TaxonomyUIFactory.class, getLocale(), getTranslator()));
 		setTranslator(Util.createPackageTranslator(RepositoryService.class, getLocale(), getTranslator()));
-		this.mapperThumbnailKey = mapperService.register(null, "repositoryentryImage", new RepositoryEntryImageMapper(450, 300));
+		// Two times the with of the card in mobile view.
+		this.mapperThumbnailKey = mapperService.register(null, "repositoryentryImage", new RepositoryEntryImageMapper(900, 600));
 		
 		mainVC = createVelocityContainer("launch_repository_entry");
 		
