57470b387d606576620fb14210d90d406bfc6bb3
srosse <stephane.rosse@frentix.com>
2023-03-01 15:50:50 +0100

    OO-6805: updated wrong alter script

--- src/main/resources/database/postgresql/alter_17_2_0_to_17_2_1.sql
+++ src/main/resources/database/postgresql/alter_17_2_0_to_17_2_1.sql
@@ -1,2 +1,8 @@
--- LTI
-alter table o_lti_tool_deployment add column l_deployment_resource_id varchar(255);
+-- Task
+alter table o_as_entry add column a_coach_assignment_date timestamp default null;
+alter table o_as_entry add column fk_coach int8 default null;
+
+alter table o_as_entry add constraint as_entry_to_coach_idx foreign key (fk_coach) references o_bs_identity (id);
+create index idx_as_entry_to_coach_idx on o_as_entry (fk_coach);
+
+alter table o_course_element add column c_coach_assignment bool default false not null;
