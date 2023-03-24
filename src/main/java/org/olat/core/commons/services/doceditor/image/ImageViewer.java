710099a0f9df4e356857d5b722ac918f47603c65
uhensler <urs.hensler@frentix.com>
2023-03-24 08:11:45 +0100

    OO-6883: ImageViewer as part of DocEditor infrastructure

---
+++ src/main/java/org/olat/core/commons/services/doceditor/image/ImageViewer.java
@@ -0,0 +1,136 @@
+/**
+ * <a href="https://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, https://www.frentix.com
+ * <p>
+ */
+package org.olat.core.commons.services.doceditor.image;
+
+import java.util.Locale;
+
+import org.olat.core.commons.services.doceditor.Access;
+import org.olat.core.commons.services.doceditor.DocEditor;
+import org.olat.core.commons.services.doceditor.DocEditorConfigs;
+import org.olat.core.commons.services.vfs.VFSMetadata;
+import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.control.Controller;
+import org.olat.core.gui.control.WindowControl;
+import org.olat.core.gui.translator.Translator;
+import org.olat.core.id.Identity;
+import org.olat.core.id.Roles;
+import org.olat.core.util.Util;
+import org.olat.core.util.vfs.VFSLeaf;
+import org.olat.modules.video.ui.VideoRuntimeController;
+import org.springframework.stereotype.Service;
+
+/**
+ * Initial date: 23 Mar 2023<br>
+ * @author uhensler, urs.hensler@frentix.com, http://www.frentix.com
+ *
+ */
+@Service
+public class ImageViewer implements DocEditor {
+
+	private static final String TYPE = "image";
+	private static final String FORMAT_JPG = "jpg";
+	private static final String FORMAT_JPEG = "jpeg";
+	private static final String FORMAT_PNG = "png";
+	private static final String FORMAT_GIF = "gif";
+
+	@Override
+	public boolean isEnable() {
+		return true;
+	}
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+
+	@Override
+	public String getDisplayName(Locale locale) {
+		Translator translator = Util.createPackageTranslator(VideoRuntimeController.class, locale);
+		return translator.translate("viewer.display.name");
+	}
+
+	@Override
+	public int getPriority() {
+		return 5;
+	}
+
+	@Override
+	public boolean isEditEnabled() {
+		// this is only a player
+		return false;
+	}
+
+	@Override
+	public boolean isCollaborative() {
+		return false;
+	}
+
+	@Override
+	public boolean isDataTransferConfirmationEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean hasDocumentBaseUrl() {
+		return false;
+	}
+
+	@Override
+	public String getDocumentBaseUrl() {
+		return null;
+	}
+
+	@Override
+	public boolean isEnabledFor(Identity identity, Roles roles) {
+		return true;
+	}
+
+	@Override
+	public boolean isSupportingFormat(String suffix, Mode mode, boolean metadataAvailable) {
+		if (Mode.EDIT == mode) return false;
+		if (FORMAT_JPG.equalsIgnoreCase(suffix)) return true;
+		if (FORMAT_JPEG.equalsIgnoreCase(suffix)) return true;
+		if (FORMAT_PNG.equalsIgnoreCase(suffix)) return true;
+		if (FORMAT_GIF.equalsIgnoreCase(suffix)) return true;
+		return false;
+	}
+
+	@Override
+	public boolean isLockedForMe(VFSLeaf vfsLeaf, Identity identity, Mode mode) {
+		return false;
+	}
+
+	@Override
+	public boolean isLockedForMe(VFSLeaf vfsLeaf, VFSMetadata metadata, Identity identity, Mode mode) {
+		return false;
+	}
+
+	@Override
+	public int getAccessDurationMinutes(Mode mode) {
+		return 60;
+	}
+
+	@Override
+	public Controller getRunController(UserRequest ureq, WindowControl wControl, Identity identity,
+			DocEditorConfigs configs, Access access) {
+		return new ImageViewerController(ureq, wControl, configs, access);
+	}
+
+}
---
+++ src/main/java/org/olat/core/commons/services/doceditor/image/ImageViewerController.java
@@ -0,0 +1,83 @@
+/**
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
+ * <p>
+ */
+package org.olat.core.commons.services.doceditor.image;
+
+import org.olat.core.commons.services.doceditor.Access;
+import org.olat.core.commons.services.doceditor.DocEditorConfigs;
+import org.olat.core.commons.services.doceditor.DocEditorService;
+import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.components.Component;
+import org.olat.core.gui.components.Window;
+import org.olat.core.gui.components.velocity.VelocityContainer;
+import org.olat.core.gui.control.Event;
+import org.olat.core.gui.control.WindowControl;
+import org.olat.core.gui.control.controller.BasicController;
+import org.olat.core.util.CodeHelper;
+import org.olat.core.util.vfs.VFSLeaf;
+import org.olat.core.util.vfs.VFSMediaMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 
+ * Initial date: 23 Mar 2023<br>
+ * @author uhensler, urs.hensler@frentix.com, http://www.frentix.com
+ *
+ */
+public class ImageViewerController extends BasicController {
+	
+	private final Access access;
+	
+	@Autowired
+	private DocEditorService docEditorService;
+
+	public ImageViewerController(UserRequest ureq, WindowControl wControl, DocEditorConfigs configs, Access access) {
+		super(ureq, wControl);
+		this.access = access;
+		
+		wControl.getWindowBackOffice().getWindow().addListener(this);
+		
+		VelocityContainer mainVC = createVelocityContainer("view");
+		putInitialPanel(mainVC);
+		
+		VFSLeaf vfsLeaf = configs.getVfsLeaf();
+		VFSMediaMapper mapper = new VFSMediaMapper(vfsLeaf);
+		String mapperId = Long.toString(CodeHelper.getUniqueIDFromString(vfsLeaf.getRelPath()));
+		String url = registerCacheableMapper(ureq, mapperId, mapper);
+		mainVC.contextPut("url", url);
+	}
+	
+	@Override
+	public void event(UserRequest ureq, Component source, Event event) {
+		if (event == Window.CLOSE_WINDOW) {
+			deleteAccess();
+		}
+	}
+	
+	@Override
+	protected void doDispose() {
+		deleteAccess();
+		super.doDispose();
+	}
+	
+	private void deleteAccess() {
+		docEditorService.deleteAccess(access);
+	}
+
+}
