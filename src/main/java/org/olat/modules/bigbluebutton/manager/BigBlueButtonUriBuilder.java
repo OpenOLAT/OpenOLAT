f4e215909ec0f81cd487cce3c54373fd0e9e8333
srosse <stephane.rosse@frentix.com>
2023-11-01 11:14:40 +0100

    OO-7388: use SHA256 to sign requests to BigBlueButton servers

--- src/main/java/org/olat/modules/bigbluebutton/manager/BigBlueButtonUriBuilder.java
+++ src/main/java/org/olat/modules/bigbluebutton/manager/BigBlueButtonUriBuilder.java
@@ -160,7 +160,7 @@ public class BigBlueButtonUriBuilder {
     private static String checksum(String s) {
     	String checksum = "";
     	try {
-    		checksum = org.apache.commons.codec.digest.DigestUtils.sha1Hex(s);
+    		checksum = org.apache.commons.codec.digest.DigestUtils.sha256Hex(s);
     	} catch (Exception e) {
     		log.error("Cannot calculate checksum", e);
     	}
