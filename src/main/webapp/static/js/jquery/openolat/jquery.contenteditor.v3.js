7c06190d9bdba3e6d1e0e9117ba0f6cf6c218925
cpfranger <christoph.pfranger@frentix.com>
2023-12-15 14:06:18 +0100

    OO-7140: Add code element to jQuery click listener

--- src/main/webapp/static/js/jquery/openolat/jquery.contenteditor.v3.js
+++ src/main/webapp/static/js/jquery/openolat/jquery.contenteditor.v3.js
@@ -165,6 +165,7 @@
 						|| jTarget.closest(".o_evaluation_editor_form").length > 0
 						|| jTarget.closest(".o_page_with_side_options_wrapper").length > 0
 						|| jTarget.closest(".o_page_table_edition").length > 0
+						|| jTarget.closest(".o_page_edit_code").length > 0
 						|| jTarget.closest("a").length > 0 || jTarget.closest("button").length > 0
 						|| e.target.nodeName == 'BODY'
 						// callout of an editor is open
