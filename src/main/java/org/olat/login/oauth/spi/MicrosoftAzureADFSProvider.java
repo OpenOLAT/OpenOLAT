01390f563dffbc662ab0b979d0ea6560c1db41fb
srosse <stephane.rosse@frentix.com>
2024-05-30 08:39:39 +0200

    OO-7510: remove all scopes for SharePoint

--- src/main/java/org/olat/login/oauth/spi/MicrosoftAzureADFSProvider.java
+++ src/main/java/org/olat/login/oauth/spi/MicrosoftAzureADFSProvider.java
@@ -131,7 +131,8 @@ public class MicrosoftAzureADFSProvider implements OAuthSPI {
 			scopes.append(" OnlineMeetings.ReadWrite");
 		}
 		if(sharePointModule.isEnabled()) {
-			scopes.append(" MyFiles.Read MyFiles.Write");
+			// Sites.Search.All AllSites.Read MyFiles.Read MyFiles.Write
+			// scopes.append(" MyFiles.Read MyFiles.Write");
 		}
 		scopes.append(" offline_access");
 		
