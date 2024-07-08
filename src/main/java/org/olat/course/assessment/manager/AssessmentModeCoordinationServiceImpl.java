125110ba1995ef29df0efdda3e903854a9f7550d
skapoor <sumit.kapoor@frentix.com>
2024-07-08 11:57:51 +0200

    OO-7499: rollback changes for ending in leadtime

--- src/main/java/org/olat/course/assessment/manager/AssessmentModeCoordinationServiceImpl.java
+++ src/main/java/org/olat/course/assessment/manager/AssessmentModeCoordinationServiceImpl.java
@@ -73,7 +73,7 @@ import org.springframework.stereotype.Service;
 /**
  * 
  * Initial date: 06.01.2015<br>
- * @author srosse, stephane.rosse@frentix.com, http://www.frentix.com
+ * @author srosse, stephane.rosse@frentix.com, https://www.frentix.com
  *
  */
 @Service
@@ -648,7 +648,7 @@ public class AssessmentModeCoordinationServiceImpl implements AssessmentModeCoor
 		if(!testSessions.isEmpty()) {
 			List<Long> testSessionKeys = testSessions.stream()
 					.map(AssessmentTestSession::getKey)
-					.collect(Collectors.toList());
+					.toList();
 			
 			Long doerKey = doer == null ? null : doer.getKey();
 			PullTestSessionsTask task = new PullTestSessionsTask(mode.getRepositoryEntry().getKey(), testSessionKeys, doerKey);
@@ -661,18 +661,10 @@ public class AssessmentModeCoordinationServiceImpl implements AssessmentModeCoor
 		if(mode.getFollowupTime() > 0) {
 			if(mode.getStatus() != Status.followup || mode.getStatus() != Status.end) {
 				Date now = new Date();
-				// if an assessment mode gets stopped while in leadtime, just end it without follow up
-				// also set begin/end time to the same, because otherwise begin would be after end
-				if (mode.getStatus().equals(Status.leadtime)) {
-					mode.setBegin(now);
-					mode.setEnd(now);
-					mode.setStatus(Status.end);
-				} else {
-					Date followupTime = assessmentModeManager.evaluateFollowupTime(now, mode.getFollowupTime());
-					mode.setEnd(now);
-					mode.setEndWithFollowupTime(followupTime);
-					mode.setStatus(Status.followup);
-				}
+				Date followupTime = assessmentModeManager.evaluateFollowupTime(now, mode.getFollowupTime());
+				mode.setEnd(now);
+				mode.setEndWithFollowupTime(followupTime);
+				mode.setStatus(Status.followup);
 			}
 			mode.setEndStatus(endStatus);
 			mode = dbInstance.getCurrentEntityManager().merge(mode);
