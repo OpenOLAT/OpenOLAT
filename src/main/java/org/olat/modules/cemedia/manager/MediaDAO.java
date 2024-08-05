ba91c58708329cd5e2f6adb6c8d07e58cb3a5eea
cpfranger <christoph.pfranger@frentix.com>
2024-08-05 13:45:07 +0200

    OO-7937: Remove reference from o_media_version to o_vfs_metadata before deleting VFS item silently

--- src/main/java/org/olat/modules/cemedia/manager/MediaDAO.java
+++ src/main/java/org/olat/modules/cemedia/manager/MediaDAO.java
@@ -867,8 +867,12 @@ public class MediaDAO {
 			VFSContainer container = fileStorage.getMediaContainer(mediaVersion);
 			VFSItem item = container.resolve(mediaVersion.getRootFilename());
 			if(item instanceof VFSLeaf leaf) {
-				leaf.deleteSilently();
 				VFSMetadata metadata = mediaVersion.getMetadata();
+				if (metadata != null) {
+					mediaVersion.setMetadata(null);
+					update(mediaVersion);
+				}
+				leaf.deleteSilently();
 				if (metadata != null && (metadata.isTranscoded() || metadata.isInTranscoding())) {
 					vfsTranscodingService.deleteMasterFile(leaf);
 				}
