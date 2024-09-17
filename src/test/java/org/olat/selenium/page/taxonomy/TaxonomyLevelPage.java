16d465fa72cbf79633b40874bca0f4484c4545ac
srosse <stephane.rosse@frentix.com>
2024-09-17 07:45:09 +0200

    OO-8078: update selenium tests

--- src/test/java/org/olat/selenium/page/taxonomy/TaxonomyLevelPage.java
+++ src/test/java/org/olat/selenium/page/taxonomy/TaxonomyLevelPage.java
@@ -89,7 +89,7 @@ public class TaxonomyLevelPage {
 		OOGraphene.moveAndClick(searchBy, browser);
 
 		// select all
-		By selectAll = By.xpath("//dialog[contains(@class,'modal')]//th/div[contains(@class,'o_table_checkall')]/a[i[contains(@class,'o_icon_check_off')]]");
+		By selectAll = By.xpath("//dialog[contains(@class,'modal')]//th/a[i[contains(@class,'o_icon_check_off')]]");
 		OOGraphene.waitElement(selectAll, browser);
 		browser.findElement(selectAll).click();
 		OOGraphene.waitBusy(browser);
