7cfb5c5be841c5b0df76ab76507eeb64cdbb5d16
srosse <stephane.rosse@frentix.com>
2024-09-24 07:36:16 +0200

    no-issue: screen shot start of assessment in selenium test

--- src/test/java/org/olat/selenium/page/course/AssessmentModePage.java
+++ src/test/java/org/olat/selenium/page/course/AssessmentModePage.java
@@ -177,11 +177,16 @@ public class AssessmentModePage {
 	 * @return
 	 */
 	public AssessmentModePage startAssessment() {
-		By startBy = By.cssSelector("div.modal-dialog div.modal-body div.o_button_group a.o_sel_assessment_start");
-		OOGraphene.waitElement(startBy, browser);
-		browser.findElement(startBy).click();
-		OOGraphene.waitBusy(browser);
-		OOGraphene.waitModalDialogDisappears(browser);
+		try {
+			By startBy = By.cssSelector("div.modal-dialog div.modal-body div.o_button_group a.o_sel_assessment_start");
+			OOGraphene.waitElement(startBy, browser);
+			browser.findElement(startBy).click();
+			OOGraphene.waitBusy(browser);
+			OOGraphene.waitModalDialogDisappears(browser);
+		} catch (Exception e) {
+			OOGraphene.takeScreenshot("Assessment_startAssessment", browser);
+			throw e;
+		}
 		return this;
 	}
 	
