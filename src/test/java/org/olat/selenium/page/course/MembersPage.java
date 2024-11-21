d5be140a3bc98da07db8739f4535fc38f3b4d112
srosse <stephane.rosse@frentix.com>
2024-11-21 07:52:05 +0100

    no-issue: update selenium tests for tools accessibility

--- src/test/java/org/olat/selenium/page/course/MembersPage.java
+++ src/test/java/org/olat/selenium/page/course/MembersPage.java
@@ -177,7 +177,7 @@ public class MembersPage {
 	 * @return Itself
 	 */
 	public MembersPage openMembership(String name) {
-		By toolBy = By.xpath("//div//tr[td/a[text()[contains(.,'" + name+ "')]]]/td/div/a[i[contains(@class,'o_icon_actions o_icon-fws')]]");
+		By toolBy = By.xpath("//div//tr[td/a[text()[contains(.,'" + name+ "')]]]/td/div/a[i[contains(@class,'o_icon-fw')][contains(@class,'o_icon_actions')]]");
 		OOGraphene.waitElement(toolBy, browser);
 		browser.findElement(toolBy).click();
 		OOGraphene.waitCallout(browser);
