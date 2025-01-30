1c64f6eb8fe68ca728656eee28229b9204243e14
srosse <stephane.rosse@frentix.com>
2025-01-30 10:30:23 +0100

    OO-8384: from curriculum management to course management

--- src/main/java/org/olat/modules/curriculum/ui/CurriculumAdminController.java
+++ src/main/java/org/olat/modules/curriculum/ui/CurriculumAdminController.java
@@ -90,8 +90,7 @@ public class CurriculumAdminController extends BasicController {
 	@Override
 	protected void event(UserRequest ureq, Component source, Event event) {
 		if(source == segmentView) {
-			if(event instanceof SegmentViewEvent) {
-				SegmentViewEvent sve = (SegmentViewEvent)event;
+			if(event instanceof SegmentViewEvent sve) {
 				String segmentCName = sve.getComponentName();
 				Component clickedLink = mainVC.getComponent(segmentCName);
 				if (clickedLink == configurationLink) {
