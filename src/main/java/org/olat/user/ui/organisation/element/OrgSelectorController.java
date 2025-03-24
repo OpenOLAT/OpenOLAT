3c4e2f4de7d37fd9c9bd7f9312860109b0ee4f97
cpfranger <christoph.pfranger@frentix.com>
2025-03-24 16:36:34 +0100

    OO-8315: New org selector dialog

---
+++ src/main/java/org/olat/user/ui/organisation/element/OrgSelectorController.java
@@ -0,0 +1,256 @@
+/**
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
+ * <p>
+ */
+package org.olat.user.ui.organisation.element;
+
+import java.io.Serial;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.components.form.flexible.FormItem;
+import org.olat.core.gui.components.form.flexible.FormItemContainer;
+import org.olat.core.gui.components.form.flexible.elements.FormLink;
+import org.olat.core.gui.components.form.flexible.elements.StaticTextElement;
+import org.olat.core.gui.components.form.flexible.elements.TextElement;
+import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
+import org.olat.core.gui.components.form.flexible.impl.FormEvent;
+import org.olat.core.gui.components.link.Link;
+import org.olat.core.gui.control.Controller;
+import org.olat.core.gui.control.Event;
+import org.olat.core.gui.control.WindowControl;
+import org.olat.core.util.StringHelper;
+
+/**
+ * Initial date: 2025-03-18<br>
+ *
+ * @author cpfranger, christoph.pfranger@frentix.com, <a href="https://www.frentix.com">https://www.frentix.com</a>
+ */
+public class OrgSelectorController extends FormBasicController {
+	private static final String PARAMETER_KEY_ORG_SELECTION = "org_sel_";
+
+	private static final int MAX_RESULTS = 50;
+
+	private FormLink applyButton;
+	private TextElement quickSearchEl;
+	private FormLink resetQuickSearchButton;
+	private StaticTextElement selectionNoneEl;
+	private StaticTextElement selectionNumEl;
+
+	private StaticTextElement resultsMoreEl;
+
+	private Set<Long> selectedKeys;
+	private final boolean multipleSelection;
+	private final List<Row> rows;
+
+	public record Row(Long key, String path, String title, String location, int size) {}
+
+	public OrgSelectorController(UserRequest ureq, WindowControl wControl, List<OrgSelectorElementImpl.OrgRow> orgRows, 
+								 Set<Long> selectedKeys, boolean multipleSelection) {
+		super(ureq, wControl, "org_selector");
+
+		this.selectedKeys = selectedKeys;
+		this.multipleSelection = multipleSelection;
+
+		rows = orgRows.stream().map(this::row).toList();
+
+		initForm(ureq);
+		doResetQuickSearch(ureq);
+	}
+
+	@Override
+	protected void initForm(FormItemContainer formLayout, Controller listener, UserRequest ureq) {
+		initSearchLine(formLayout);
+
+		selectionNoneEl = uifactory.addStaticTextElement("selector.selection.none",
+				"selector.selection", translate("selector.selection.none"), formLayout);
+
+		selectionNumEl = uifactory.addStaticTextElement("selector.selection.num",
+				"selector.selection.num", "", formLayout);
+
+		resultsMoreEl = uifactory.addStaticTextElement("selector.results.more", null,
+				translate("selector.results.more", String.valueOf(MAX_RESULTS)), formLayout);
+
+		applyButton = uifactory.addFormLink("apply", formLayout, Link.BUTTON_SMALL);
+		applyButton.setPrimary(true);
+	}
+
+	private void initSearchLine(FormItemContainer formLayout) {
+		FormLink quickSearchButton = uifactory.addFormLink("quickSearchButton", "", null, formLayout,
+				Link.BUTTON | Link.NONTRANSLATED);
+		quickSearchButton.setElementCssClass("o_indicate_search");
+		quickSearchButton.setIconLeftCSS("o_icon o_icon_search");
+		quickSearchButton.setEnabled(false);
+		quickSearchButton.setDomReplacementWrapperRequired(false);
+		quickSearchButton.setTitle(translate("search"));
+
+		quickSearchEl = uifactory.addTextElement("quickSearch", null, 32, "", formLayout);
+		quickSearchEl.setPlaceholderKey("enter.search.term", null);
+		quickSearchEl.setElementCssClass("o_quick_search");
+		quickSearchEl.setDomReplacementWrapperRequired(false);
+		quickSearchEl.addActionListener(FormEvent.ONKEYUP);
+		quickSearchEl.setFocus(true);
+		quickSearchEl.setAriaLabel(translate("enter.search.term"));
+
+		resetQuickSearchButton = uifactory.addFormLink("resetQuickSearch", "", null, formLayout,
+				Link.BUTTON | Link.NONTRANSLATED);
+		resetQuickSearchButton.setElementCssClass("o_reset_search");
+		resetQuickSearchButton.setIconLeftCSS("o_icon o_icon_remove_filters");
+		resetQuickSearchButton.setDomReplacementWrapperRequired(false);
+		resetQuickSearchButton.setTitle(translate("reset"));
+	}
+
+	private Row row(OrgSelectorElementImpl.OrgRow orgRow) {
+		Long key = orgRow.key();
+		String path = orgRow.path();
+		String title = orgRow.title();
+		String location = orgRow.location();
+		int size = 1;
+		return new Row(key, path, title, location, size);
+	}
+
+	@Override
+	protected void formInnerEvent(UserRequest ureq, FormItem source, FormEvent event) {
+		if (applyButton == source) {
+			doApply(ureq);
+		} else if (quickSearchEl == source) {
+			doQuickSearch(ureq);
+		} else if (resetQuickSearchButton == source) {
+			doResetQuickSearch(ureq);			
+		} else if (flc == source) {
+			String unselect = ureq.getParameter("unselect");
+			String select = ureq.getParameter("select");
+			if (unselect != null) {
+				Long orgKey = Long.valueOf(unselect);
+				if (selectedKeys.contains(orgKey)) {
+					selectedKeys.remove(orgKey);
+					updateUI();
+				}
+			} else if (select != null) {
+				Long orgKey = Long.valueOf(select);
+				if (!selectedKeys.contains(orgKey)) {
+					if (multipleSelection) {
+						selectedKeys.add(orgKey);
+					} else {
+						selectedKeys = new HashSet<>();
+						selectedKeys.add(orgKey);
+					}
+					updateUI();
+				}
+			}
+		}
+		super.formInnerEvent(ureq, source, event);
+	}
+
+	@Override
+	protected void propagateDirtinessToContainer(FormItem source, FormEvent fe) {
+	}
+
+	@Override
+	protected void formOK(UserRequest ureq) {
+		doApply(ureq);
+	}
+
+	@Override
+	protected void formCancelled(UserRequest ureq) {
+		fireEvent(ureq, Event.CANCELLED_EVENT);
+	}
+
+	private void doApply(UserRequest ureq) {
+		HashSet<Long> selectedKeys = new HashSet<>();
+		for (String name : ureq.getParameterSet()) {
+			if (name.startsWith(PARAMETER_KEY_ORG_SELECTION)) {
+				String keyString = name.substring(PARAMETER_KEY_ORG_SELECTION.length());
+				selectedKeys.add(Long.parseLong(keyString));
+			}
+		}
+		this.selectedKeys = selectedKeys;
+		fireEvent(ureq, new OrgsSelectedEvent(selectedKeys));
+	}
+	
+	private void doQuickSearch(UserRequest ureq) {
+		updateUI();
+		
+		fireEvent(ureq, RESIZED_EVENT);
+	}
+	
+	private void doResetQuickSearch(UserRequest ureq) {
+		quickSearchEl.setValue("");
+		updateUI();
+		
+		fireEvent(ureq, RESIZED_EVENT);
+	}
+	
+	private void updateUI() {
+		resultsMoreEl.setVisible(false);
+
+		List<Row> selectedRows = rows.stream().filter(row -> selectedKeys.contains(row.key)).toList();
+		flc.contextPut("selectedRows", selectedRows);
+
+		selectionNoneEl.setVisible(selectedKeys.isEmpty());
+		selectionNumEl.setVisible(!selectedKeys.isEmpty());
+		if (multipleSelection) {
+			selectionNumEl.setLabel("selector.selection.num", new String[] { String.valueOf(selectedKeys.size()) });
+		} else {
+			selectionNumEl.setLabel("selector.selection", null);
+		}
+
+		quickSearchEl.getComponent().setDirty(false);
+
+		String searchFieldValue = quickSearchEl.getValue().toLowerCase();
+		if (StringHelper.containsNonWhitespace(searchFieldValue)) {
+			List<Row> unselectedRows = rows.stream()
+					.filter(row -> !selectedKeys.contains(row.key))
+					.filter(row -> row.title.toLowerCase().contains(searchFieldValue))
+					.toList();
+			
+			if (unselectedRows.size() > MAX_RESULTS) {
+				unselectedRows = unselectedRows.subList(0, MAX_RESULTS);
+				resultsMoreEl.setVisible(true);
+			}
+
+			flc.contextPut("unselectedRows", unselectedRows);
+		} else {
+			flc.contextPut("unselectedRows", Collections.emptyList());
+		}
+		
+		resultsMoreEl.getComponent().setDirty(true);
+	}
+	
+	public static class OrgsSelectedEvent extends Event {
+
+		@Serial
+		private static final long serialVersionUID = -2594865379910201039L;
+
+		private final Set<Long> keys;
+
+		public OrgsSelectedEvent(Set<Long> keys) {
+			super("orgs-selected");
+			this.keys = keys;
+		}
+
+		public Set<Long> getKeys() {
+			return keys;
+		}
+	}
+	
+	public static final Event RESIZED_EVENT = new Event("org-selector-resized");
+}
---
+++ src/main/java/org/olat/user/ui/organisation/element/OrgSelectorElementImpl.java
@@ -0,0 +1,257 @@
+/**
+ * <a href="http://www.openolat.org">
+ * OpenOLAT - Online Learning and Training</a><br>
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); <br>
+ * you may not use this file except in compliance with the License.<br>
+ * You may obtain a copy of the License at the
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
+ * <p>
+ * Unless required by applicable law or agreed to in writing,<br>
+ * software distributed under the License is distributed on an "AS IS" BASIS, <br>
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
+ * See the License for the specific language governing permissions and <br>
+ * limitations under the License.
+ * <p>
+ * Initial code contributed and copyrighted by<br>
+ * frentix GmbH, http://www.frentix.com
+ * <p>
+ */
+package org.olat.user.ui.organisation.element;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.olat.core.gui.UserRequest;
+import org.olat.core.gui.components.Component;
+import org.olat.core.gui.components.form.flexible.FormItem;
+import org.olat.core.gui.components.form.flexible.FormItemCollection;
+import org.olat.core.gui.components.form.flexible.elements.FormLink;
+import org.olat.core.gui.components.form.flexible.impl.Form;
+import org.olat.core.gui.components.form.flexible.impl.FormItemImpl;
+import org.olat.core.gui.components.form.flexible.impl.FormJSHelper;
+import org.olat.core.gui.components.form.flexible.impl.elements.FormLinkImpl;
+import org.olat.core.gui.components.link.Link;
+import org.olat.core.gui.control.Controller;
+import org.olat.core.gui.control.ControllerEventListener;
+import org.olat.core.gui.control.Event;
+import org.olat.core.gui.control.WindowControl;
+import org.olat.core.gui.control.generic.closablewrapper.CalloutSettings;
+import org.olat.core.gui.control.generic.closablewrapper.CloseableCalloutWindowController;
+import org.olat.core.gui.control.winmgr.Command;
+import org.olat.core.gui.translator.Translator;
+import org.olat.core.id.Organisation;
+import org.olat.core.util.StringHelper;
+import org.olat.core.util.Util;
+
+/**
+ * Initial date: 2025-03-18<br>
+ *
+ * @author cpfranger, christoph.pfranger@frentix.com, <a href="https://www.frentix.com">https://www.frentix.com</a>
+ */
+public class OrgSelectorElementImpl extends FormItemImpl implements OrgSelectorElement, FormItemCollection, ControllerEventListener {
+
+	private final OrgSelectorComponent component;
+	private final WindowControl wControl;
+	private final Map<String, FormItem> components = new HashMap<>();
+	private final FormLink button;
+	private List<OrgRow> orgRows;
+
+	private OrgSelectorController orgSelectorCtrl;
+	private CloseableCalloutWindowController calloutCtrl;
+
+	private Set<Long> selectedKeys = new HashSet<>();
+	private boolean multipleSelection;
+	private Translator orgTranslator;
+
+	public OrgSelectorElementImpl(WindowControl wControl, String name, List<Organisation> orgs) {
+		super(name);
+		initOrgRows(orgs);
+		this.wControl = wControl;
+		this.component = new OrgSelectorComponent(name, this);
+		String dispatchId = component.getDispatchID();
+		String buttonId = dispatchId + "_org_sel";
+		button = new FormLinkImpl(buttonId, buttonId, "", Link.BUTTON | Link.NONTRANSLATED);
+		button.setDomReplacementWrapperRequired(false);
+		button.setTranslator(translator);
+		button.setElementCssClass("o_org_selector_button o_can_have_focus");
+		button.getComponent().setLabelCSS("o_org_selector_span");
+		button.setIconRightCSS("o_icon o_icon_caret");
+		components.put(buttonId, button);
+		rootFormAvailable(button);
+	}
+
+	private void initOrgRows(List<Organisation> orgs) {
+		Map<Long, String> orgKeyToName = new HashMap<>();
+		for (Organisation org : orgs) {
+			orgKeyToName.put(org.getKey(), org.getDisplayName());
+		}
+		orgRows = orgs.stream().map(org -> mapToOrgRow(org, orgKeyToName)).collect(Collectors.toList());
+	}
+
+	private OrgRow mapToOrgRow(Organisation org, Map<Long, String> orgKeyToName) {
+		Long key = org.getKey();
+		String path = Arrays.stream(org.getMaterializedPathKeys().split("/")).map(String::trim)
+				.filter(StringHelper::containsNonWhitespace)
+				.map(Long::parseLong).map(orgKeyToName::get).collect(Collectors.joining(" / "));
+		String title = org.getDisplayName();
+		String location = org.getLocation();
+		
+		return new OrgRow(key, path, title, location);
+	}
+
+	@Override
+	public void setSelection(Collection<Long> orgKeys) {
+		if (!multipleSelection && orgKeys.size() > 1) {
+			throw new AssertionError("Trying to select multiple organisations with multiple selection turned off");
+		}
+		selectedKeys = orgKeys == null ? new HashSet<>() : new HashSet<>(orgKeys);
+		updateButtonUI();
+	}
+
+	@Override
+	public void setSelection(Long orgKey) {
+		setSelection(Set.of(orgKey));
+	}
+
+	@Override
+	public Set<Long> getSelection() {
+		return selectedKeys;
+	}
+
+	public FormLink getButton() {
+		return button;
+	}
+
+	@Override
+	public Iterable<FormItem> getFormItems() {
+		return new ArrayList<>(components.values());
+	}
+
+	@Override
+	public FormItem getFormComponent(String name) {
+		return components.get(name);
+	}
+
+	@Override
+	protected Component getFormItemComponent() {
+		return component;
+	}
+
+	@Override
+	protected void rootFormAvailable() {
+		rootFormAvailable(button);
+	}
+
+	private void rootFormAvailable(FormLink button) {
+		if (button != null && button.getRootForm() != getRootForm()) {
+			button.setRootForm(getRootForm());
+		}
+	}
+
+	@Override
+	public void reset() {
+		//
+	}
+
+	@Override
+	public void evalFormRequest(UserRequest ureq) {
+		Form form = getRootForm();
+		String dispatchuri = form.getRequestParameter("dispatchuri");
+		if (button != null && button.getFormDispatchId().equalsIgnoreCase(dispatchuri)) {
+			doOpenSelector(ureq);
+		}
+	}
+
+	@Override
+	public void dispatchEvent(UserRequest ureq, Controller source, Event event) {
+		if (orgSelectorCtrl == source) {
+			if (event instanceof OrgSelectorController.OrgsSelectedEvent orgsSelectedEvent) {
+				selectedKeys = orgsSelectedEvent.getKeys();
+				calloutCtrl.deactivate();
+				cleanUp();
+				updateButtonUI();
+				Command focusCommand = FormJSHelper.getFormFocusCommand(getRootForm().getFormName(), button.getForId());
+				getRootForm().getWindowControl().getWindowBackOffice().sendCommandTo(focusCommand);
+			} else if (event == OrgSelectorController.RESIZED_EVENT) {
+				calloutCtrl.handleResized();
+			} else if (event == Event.CANCELLED_EVENT) {
+				if (calloutCtrl != null) {
+					calloutCtrl.deactivate();
+				}
+				cleanUp();
+			}
+		} else if (calloutCtrl == source) {
+			cleanUp();
+		}
+	}
+	
+	private void cleanUp() {
+		orgSelectorCtrl = cleanUp(orgSelectorCtrl);
+		calloutCtrl = cleanUp(calloutCtrl);
+	}
+	
+	private <T extends Controller> T cleanUp(T ctrl) {
+		if (ctrl != null) {
+			ctrl.removeControllerListener(this);
+			ctrl = null;
+		}
+		return ctrl;
+	}
+	
+	private void updateButtonUI() {
+		boolean noOrgSelected = false;
+		
+		String linkTitle = orgRows.stream()
+				.filter(orgRow -> selectedKeys.contains(orgRow.key()))
+				.map(OrgRow::title)
+				.collect(Collectors.joining(", "));
+
+		if (!StringHelper.containsNonWhitespace(linkTitle)) {
+			linkTitle = "&nbsp;";
+			noOrgSelected = true;
+		}
+		if (button != null) {
+			button.setI18nKey(linkTitle);
+			if (noOrgSelected) {
+				if (orgTranslator == null) {
+					if (getTranslator() != null) {
+						orgTranslator = Util.createPackageTranslator(OrgSelectorElementImpl.class, getTranslator().getLocale());
+					}
+				}
+				if (orgTranslator != null) {
+					button.setI18nKey(orgTranslator.translate("selector.none"));
+				}
+			}
+			component.setDirty(true);
+		}
+	}
+	
+	private void doOpenSelector(UserRequest ureq) {
+		orgSelectorCtrl = new OrgSelectorController(ureq, wControl, orgRows, selectedKeys, multipleSelection);
+		orgSelectorCtrl.addControllerListener(this);
+
+		calloutCtrl = new CloseableCalloutWindowController(ureq, wControl, orgSelectorCtrl.getInitialComponent(),
+				button.getFormDispatchId(), "", true, "",
+				new CalloutSettings(false, CalloutSettings.CalloutOrientation.bottomOrTop, false, null));
+		calloutCtrl.addControllerListener(this);
+		calloutCtrl.activate();
+	}
+
+	public void setMultipleSelection(boolean multipleSelection) {
+		this.multipleSelection = multipleSelection;
+	}
+
+	public boolean isMultipleSelection() {
+		return multipleSelection;
+	}
+
+	public record OrgRow(long key, String path, String title, String location) {}
+}
