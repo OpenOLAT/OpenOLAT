547b0b3c72b0a77c2b17b144c340938148abb131
srosse <stephane.rosse@frentix.com>
2025-05-20 10:40:54 +0200

    no-issue: wording and unused import

--- src/main/java/org/olat/user/ui/admin/UserRolesController.java
+++ src/main/java/org/olat/user/ui/admin/UserRolesController.java
@@ -26,7 +26,6 @@
 package org.olat.user.ui.admin;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
