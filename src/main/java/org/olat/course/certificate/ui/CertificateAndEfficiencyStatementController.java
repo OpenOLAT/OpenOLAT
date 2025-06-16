04b93affd5326c3a4792b0d7b971dfd7ab7364ec
uhensler <urs.hensler@frentix.com>
2025-06-16 07:38:53 +0200

    OO-8740: Course owner can always delete a certificate

--- src/main/java/org/olat/course/certificate/ui/CertificateAndEfficiencyStatementController.java
+++ src/main/java/org/olat/course/certificate/ui/CertificateAndEfficiencyStatementController.java
@@ -237,7 +237,7 @@ public class CertificateAndEfficiencyStatementController extends BasicController
 		}
 	
 		certificatesCtrl = new IdentityCertificatesController(ureq, getWindowControl(),
-				courseRepoEntry, certificateConfig, statementOwner, true);
+				courseRepoEntry, certificateConfig, statementOwner, false, true);
 		listenTo(certificatesCtrl);
 		mainVC.put("certificates", certificatesCtrl.getInitialComponent());
 		if(courseRepoEntry == null && certificate != null) {
