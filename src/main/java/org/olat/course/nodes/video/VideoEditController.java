74a3f6555db5acb381e0b9cf1573b8b9b080c5fe
cpfranger <christoph.pfranger@frentix.com>
2025-08-21 17:50:28 +0200

    OO-8853: Simplify and standardize video configuration UI and localization strings

--- src/main/java/org/olat/course/nodes/video/VideoEditController.java
+++ src/main/java/org/olat/course/nodes/video/VideoEditController.java
@@ -19,10 +19,13 @@
  */
 package org.olat.course.nodes.video;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.components.Component;
+import org.olat.core.gui.components.emptystate.EmptyStateConfig;
 import org.olat.core.gui.components.form.flexible.FormItem;
 import org.olat.core.gui.components.form.flexible.FormItemContainer;
 import org.olat.core.gui.components.form.flexible.elements.MultipleSelectionElement;
@@ -33,9 +36,7 @@ import org.olat.core.gui.components.form.flexible.elements.StaticTextElement;
 import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
 import org.olat.core.gui.components.form.flexible.impl.FormEvent;
 import org.olat.core.gui.components.form.flexible.impl.FormLayoutContainer;
-import org.olat.core.gui.components.link.Link;
-import org.olat.core.gui.components.link.LinkFactory;
-import org.olat.core.gui.components.panel.Panel;
+import org.olat.core.gui.components.panel.IconPanelLabelTextContent;
 import org.olat.core.gui.components.tabbedpane.TabbedPane;
 import org.olat.core.gui.components.util.SelectionValues;
 import org.olat.core.gui.components.velocity.VelocityContainer;
@@ -51,18 +52,23 @@ import org.olat.core.util.StringHelper;
 import org.olat.core.util.filter.FilterFactory;
 import org.olat.core.util.vfs.VFSContainer;
 import org.olat.core.util.vfs.VFSContainerMapper;
+import org.olat.course.editor.CourseNodeReferenceProvider;
 import org.olat.course.editor.NodeEditController;
 import org.olat.course.nodes.VideoCourseNode;
+import org.olat.course.nodes.videotask.ui.ConfirmChangeVideoController;
 import org.olat.fileresource.types.VideoFileResource;
 import org.olat.modules.ModuleConfiguration;
 import org.olat.modules.video.VideoManager;
+import org.olat.modules.video.VideoSegments;
 import org.olat.modules.video.ui.VideoDisplayController;
 import org.olat.modules.video.ui.VideoDisplayOptions;
 import org.olat.repository.RepositoryEntry;
 import org.olat.repository.RepositoryManager;
-import org.olat.repository.controllers.ReferencableEntriesSearchController;
+import org.olat.repository.RepositoryService;
 import org.olat.repository.handlers.RepositoryHandler;
 import org.olat.repository.handlers.RepositoryHandlerFactory;
+import org.olat.repository.ui.RepositoryEntryReferenceController;
+import org.olat.repository.ui.RepositoryEntryReferenceProvider.ReferenceContentProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -71,7 +77,9 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author dfakae, dirk.furrer@frentix.com, http://www.frentix.com
  *
  */
-public class VideoEditController extends ActivateableTabbableDefaultController implements ControllerEventListener {
+public class VideoEditController extends ActivateableTabbableDefaultController implements ControllerEventListener, ReferenceContentProvider {
+
+	private static final List<String> RESOURCE_TYPES = List.of(VideoFileResource.TYPE_NAME);
 
 	public static final String PANE_TAB_VIDEOCONFIG = "pane.tab.videoconfig";
 	private static final String[] paneKeys = { PANE_TAB_VIDEOCONFIG};
@@ -79,9 +87,6 @@ public class VideoEditController extends ActivateableTabbableDefaultController i
 	public static final String NLS_ERROR_VIDEOREPOENTRYMISSING = "error.videorepoentrymissing";
 	public static final String NLS_ERROR_VIDEOREPOENTRYDELETED = "error.videorepoentrydeleted";
 	public static final String NLS_ERROR_VIDEOREPOENTRYRESTRICTED = "error.videorepoentryrestricted";
-	private static final String NLS_COMMAND_CHOOSEVIDEO = "command.choosevideo";
-	private static final String NLS_COMMAND_CREATEVID = "command.createvideo";
-	private static final String NLS_COMMAND_CHANGEVID = "command.changevideo";
 
 	public static final String CONFIG_KEY_REPOSITORY_SOFTKEY = "reporef";
 	public static final String CONFIG_KEY_AUTOPLAY = "autoplay";
@@ -102,84 +107,62 @@ public class VideoEditController extends ActivateableTabbableDefaultController i
 	
 	public static final String CONFIG_KEY_DESCRIPTION_CUSTOMTEXT = "descriptionText";
 
-	private static final String VC_CHOSENVIDEO = "chosenvideo";
-	private static final String NLS_NO_VIDEO_CHOSEN = "no.video.chosen";
-
-	private Link previewLink;
-	private Link chooseVideoButton;
-	private Link changeVideoButton;
-	
-	private Panel main;
 	private TabbedPane myTabbedPane;
-	private VelocityContainer videoConfigurationVc;
+	private final VelocityContainer configurationVC;
+	private final IconPanelLabelTextContent iconPanelContent;
 
-	private final VideoCourseNode videoNode;
 	private final ModuleConfiguration config;
-	private RepositoryEntry repositoryEntry;
+	private final VideoCourseNode videoNode;
 
 	private CloseableModalController cmc;
 	private VideoDisplayController previewCtrl;
 	private VideoOptionsForm videoOptionsCtrl;
-	private ReferencableEntriesSearchController searchController;
+	private final RepositoryEntryReferenceController referenceCtrl;
+	private ConfirmChangeVideoController confirmChangeVideoCtrl;
 
 	@Autowired
 	private VideoManager videoManager;
+	@Autowired
+	private RepositoryService repositoryService;
 
 	public VideoEditController(VideoCourseNode videoNode, UserRequest ureq, WindowControl wControl) {
 		super(ureq, wControl);
-		this.videoNode = videoNode;
-		this.config = videoNode.getModuleConfiguration();
 		
-		main = new Panel("videomain");
+		this.videoNode = videoNode;
+		config = videoNode.getModuleConfiguration();
+		configurationVC = createVelocityContainer("edit");
+		
+		iconPanelContent = new IconPanelLabelTextContent("content");
 
-		videoConfigurationVc = createVelocityContainer("edit");
-		chooseVideoButton = LinkFactory.createButtonSmall(NLS_COMMAND_CREATEVID, videoConfigurationVc, this);
-		chooseVideoButton.setElementCssClass("o_sel_video_choose_repofile");
-		changeVideoButton = LinkFactory.createButtonSmall(NLS_COMMAND_CHANGEVID, videoConfigurationVc, this);
-		changeVideoButton.setElementCssClass("o_sel_video_change_repofile");
+		RepositoryEntry videoEntry = getVideoReference(config, false);
+		String selectionTitle = translate("select.video.learning.resource");
+		EmptyStateConfig emptyStateConfig = EmptyStateConfig.builder()
+				.withMessageTranslated(translate("no.video.resource.selected"))
+				.withIconCss("o_icon o_FileResource-MOVIE_icon").build();
+		CourseNodeReferenceProvider referenceProvider = new CourseNodeReferenceProvider(repositoryService,
+				RESOURCE_TYPES, emptyStateConfig, selectionTitle, this);
+		referenceCtrl = new RepositoryEntryReferenceController(ureq, wControl, videoEntry, referenceProvider);
+		listenTo(referenceCtrl);
+		configurationVC.put("reference", referenceCtrl.getInitialComponent());
 
-		if (config.get(CONFIG_KEY_REPOSITORY_SOFTKEY) != null) {
-			// fetch repository entry to display the repository entry title of
-			// the chosen cp
-			repositoryEntry = getVideoReference(config, false);
-			if (repositoryEntry == null) {
-				// we cannot display the entries name, because the repository
-				// entry had been deleted between the time when it was chosen
-				// here, and now
-				showError(NLS_ERROR_VIDEOREPOENTRYMISSING);
-				videoConfigurationVc.contextPut("showPreviewButton", Boolean.FALSE);
-				videoConfigurationVc.contextPut(VC_CHOSENVIDEO, translate("no.video.chosen"));
-			} else if (videoManager.isRestrictedDomain(repositoryEntry)) {
-				videoConfigurationVc.contextPut("showPreviewButton", Boolean.FALSE);
-				videoConfigurationVc.contextPut(VC_CHOSENVIDEO, translate(NLS_ERROR_VIDEOREPOENTRYRESTRICTED));
-			} else {
-				boolean restrictEdit = videoManager.isInUse(repositoryEntry);
-				videoConfigurationVc.contextPut("restrictEdit", restrictEdit);
-				videoConfigurationVc.contextPut("showPreviewButton", Boolean.TRUE);
-				String displayname = StringHelper.escapeHtml(repositoryEntry.getDisplayname());
-				previewLink = LinkFactory.createCustomLink("command.preview", "command.preview", displayname, Link.NONTRANSLATED, videoConfigurationVc, this);
-				previewLink.setTitle(getTranslator().translate("command.preview"));
-				previewLink.setIconLeftCSS("o_icon o_icon-fw o_icon_preview");
-				previewLink.setEnabled(true);
-			}
-			videoConfigurationVc.contextPut("showOptions", Boolean.TRUE);
-			VideoOptionsForm videoOptions = new VideoOptionsForm(ureq, getWindowControl(), repositoryEntry, config);
-			videoConfigurationVc.put("videoOptions", videoOptions.getInitialComponent());
-			listenTo(videoOptions);
-		} else {
-			// no valid config yet
-			videoConfigurationVc.contextPut("showPreviewButton", Boolean.FALSE);
-			videoConfigurationVc.contextPut("showOptions", Boolean.FALSE);
-			videoConfigurationVc.contextPut(VC_CHOSENVIDEO, translate(NLS_NO_VIDEO_CHOSEN));
-		}
+		putInitialPanel(configurationVC);
+		updateEditController(ureq, videoEntry, false);
+	}
 
-		main.setContent(videoConfigurationVc);
+	@Override
+	public Component getContent(RepositoryEntry repositoryEntry) {
+		return iconPanelContent;
+	}
+
+	@Override
+	public void refresh(Component cmp, RepositoryEntry repositoryEntry) {
+		//
 	}
 
 	@Override
 	public void addTabs(TabbedPane tabbedPane) {
 		myTabbedPane = tabbedPane;
-		tabbedPane.addTab(translate(PANE_TAB_VIDEOCONFIG), "o_sel_video_configuration", main);
+		tabbedPane.addTab(translate(PANE_TAB_VIDEOCONFIG), "o_sel_video_configuration", configurationVC);
 	}
 
 	@Override
@@ -193,108 +176,144 @@ public class VideoEditController extends ActivateableTabbableDefaultController i
 	}
 
 	@Override
-	protected void event(UserRequest ureq, Component source, Event event) {
-		if (source == chooseVideoButton || source == changeVideoButton) {
-			doSearch(ureq);
-		} else if(source == previewLink) {
-			doPreview(ureq);
-		}
-	}
-
-	@Override
-	public void event(UserRequest urequest, Controller source, Event event) {
-		if (source == searchController) {
-			if (event == ReferencableEntriesSearchController.EVENT_REPOSITORY_ENTRY_SELECTED) {
-				// -> close closeable modal controller
-				doSelectResource(urequest, searchController.getSelectedEntry());
-				cmc.deactivate();
-				cleanUp();
+	public void event(UserRequest ureq, Controller source, Event event) {
+		if (source == referenceCtrl) {
+			if (event == RepositoryEntryReferenceController.SELECTION_EVENT) {
+				RepositoryEntry newEntry = referenceCtrl.getRepositoryEntry();
+				RepositoryEntry currentEntry = getVideoReference(config, false);
+				referenceCtrl.setRepositoryEntry(ureq, currentEntry);
+				doConfirmChange(ureq, newEntry, currentEntry);
+			} else if (event == RepositoryEntryReferenceController.PREVIEW_EVENT) {
+				doPreview(ureq);
 			}
+		} else if (source == videoOptionsCtrl) {
+			if (event == NodeEditController.NODECONFIG_CHANGED_EVENT){
+				fireEvent(ureq, NodeEditController.NODECONFIG_CHANGED_EVENT);
+			}
+		} else if (source == confirmChangeVideoCtrl) {
+			if (event == Event.DONE_EVENT) {
+				doChangeResource(ureq, confirmChangeVideoCtrl.getNewVideoEntry());
+			}
+			cmc.deactivate();
+			cleanUp();
 		} else if(cmc == source) {
 			cleanUp();
 		}
-
-		if (event == NodeEditController.NODECONFIG_CHANGED_EVENT){
-			fireEvent(urequest, NodeEditController.NODECONFIG_CHANGED_EVENT);
-		}
 	}
-	
+
 	private void cleanUp() {
-		removeAsListenerAndDispose(searchController);
-		removeAsListenerAndDispose(previewCtrl);
+		removeAsListenerAndDispose(confirmChangeVideoCtrl);
 		removeAsListenerAndDispose(cmc);
-		searchController = null;
-		previewCtrl = null;
+		confirmChangeVideoCtrl = null;
 		cmc = null;
 	}
-	
-	private void doSearch(UserRequest ureq) {
-		removeAsListenerAndDispose(cmc);
-		removeAsListenerAndDispose(searchController);
-		
-		searchController = new ReferencableEntriesSearchController(getWindowControl(), ureq,
-				new String[] {VideoFileResource.TYPE_NAME}, translate(NLS_COMMAND_CHOOSEVIDEO),
-				true, false, false, false, false, false);
-		listenTo(searchController);
 
-		cmc = new CloseableModalController(getWindowControl(), translate("close"), searchController.getInitialComponent(), true, translate(NLS_COMMAND_CHOOSEVIDEO));
-		listenTo(cmc);
-		cmc.activate();
+	@Override
+	protected void event(UserRequest ureq, Component source, Event event) {
+		//
 	}
-	
-	private void doSelectResource(UserRequest ureq, RepositoryEntry entry) {
-		repositoryEntry = entry;
-		if (videoManager.isRestrictedDomain(repositoryEntry)) {
-			showWarning(NLS_ERROR_VIDEOREPOENTRYRESTRICTED);
-		} else if (repositoryEntry != null) {
-			setVideoReference(repositoryEntry, config);
-			videoConfigurationVc.contextPut("showPreviewButton", Boolean.TRUE);
-			String displayname = StringHelper.escapeHtml(repositoryEntry.getDisplayname());
-			previewLink = LinkFactory.createCustomLink("command.preview", "command.preview", displayname, Link.NONTRANSLATED, videoConfigurationVc, this);
-			previewLink.setIconLeftCSS("o_icon o_icon-fw o_icon_preview");
-			previewLink.setTitle(getTranslator().translate("command.preview"));
-			// fire event so the updated config is saved by the editormaincontroller
-			fireEvent(ureq, NodeEditController.NODECONFIG_CHANGED_EVENT);
 
-			videoConfigurationVc.contextPut("showOptions", Boolean.TRUE);
-			
-			removeAsListenerAndDispose(videoOptionsCtrl);
-			videoOptionsCtrl = new VideoOptionsForm(ureq, getWindowControl(), repositoryEntry, config);
-			videoConfigurationVc.put("videoOptions", videoOptionsCtrl.getInitialComponent());
+	private void doConfirmChange(UserRequest ureq, RepositoryEntry newEntry, RepositoryEntry currentEntry) {
+		if (currentEntry == null || currentEntry.equals(newEntry)) {
+			doChangeResource(ureq, newEntry);
+		} else {
+			confirmChangeVideoCtrl = new ConfirmChangeVideoController(ureq, getWindowControl(), newEntry);
+			listenTo(confirmChangeVideoCtrl);
+			String title = translate("replace.video");
+			cmc = new CloseableModalController(getWindowControl(), translate("close"), 
+					confirmChangeVideoCtrl.getInitialComponent(), title);
+			listenTo(cmc);
+			cmc.activate();
+		}
+	}
+
+	private void doChangeResource(UserRequest ureq, RepositoryEntry newEntry) {
+		doVideoReference(ureq, newEntry);
+		updateEditController(ureq, newEntry, true);
+	}
+
+	private void updateEditController(UserRequest ureq, RepositoryEntry videoEntry, boolean replacedVideo) {
+		removeAsListenerAndDispose(videoOptionsCtrl);
+		videoOptionsCtrl = null;
+
+		if (videoEntry == null) {
+			configurationVC.contextPut("showOptions", Boolean.FALSE);
+		} else {
+			updateReferenceContentUI(videoEntry);
+
+			configurationVC.contextPut("showOptions", Boolean.TRUE);
+			videoOptionsCtrl = new VideoOptionsForm(ureq, getWindowControl(), videoEntry, config);
+			configurationVC.put("videoOptions", videoOptionsCtrl.getInitialComponent());
 			listenTo(videoOptionsCtrl);
 		}
 	}
-	
+
+	private void updateReferenceContentUI(RepositoryEntry videoEntry) {
+		List<IconPanelLabelTextContent.LabelText> labelTexts = new ArrayList<>(4);
+
+		VideoSegments segments = videoManager.loadSegments(videoEntry.getOlatResource());
+		int numOfSegments = 0;
+		int numOfTerms = 0;
+		if (segments != null) {
+			numOfSegments = segments.getSegments() == null ? 0 : segments.getSegments().size();
+			numOfTerms = segments.getCategories() == null ? 0 : segments.getCategories().size();
+		}
+		labelTexts.add(new IconPanelLabelTextContent.LabelText(translate("video.terms"), Integer.toString(numOfTerms)));
+		labelTexts.add(new IconPanelLabelTextContent.LabelText(translate("video.segments"), Integer.toString(numOfSegments)));
+
+		iconPanelContent.setLabelTexts(labelTexts);
+
+		String warning = videoManager.isInUse(videoEntry) ? translate("error.edit.restricted.in.use") : null;
+		iconPanelContent.setWarning(warning);
+	}
+
+
+	private void doVideoReference(UserRequest ureq, RepositoryEntry newEntry) {
+		if (newEntry == null) {
+			return;
+		}
+
+		referenceCtrl.setRepositoryEntry(ureq, newEntry);
+
+		setVideoReference(newEntry, config);
+		fireEvent(ureq, NodeEditController.NODECONFIG_CHANGED_EVENT);
+	}
+
 	private void doPreview(UserRequest ureq) {
 		removeAsListenerAndDispose(cmc);
 		removeAsListenerAndDispose(previewCtrl);
 		
+		RepositoryEntry repositoryEntry = getVideoReference(config, false);
+		if (videoManager.isRestrictedDomain(repositoryEntry)) {
+			showWarning(NLS_ERROR_VIDEOREPOENTRYRESTRICTED);
+			return;
+		}
 		VideoDisplayOptions options = videoNode.getVideoDisplay(repositoryEntry, true);
 		previewCtrl = new VideoDisplayController(ureq, getWindowControl(), repositoryEntry, null, null, options);
 		listenTo(previewCtrl);
-		cmc = new CloseableModalController(getWindowControl(), translate("close"), previewCtrl.getInitialComponent(), true, translate(NLS_COMMAND_CHOOSEVIDEO));
+		cmc = new CloseableModalController(getWindowControl(), translate("close"), previewCtrl.getInitialComponent(), 
+				true, translate("command.preview"));
 		listenTo(cmc);
 		cmc.activate();
 	}
 
 	public static RepositoryEntry getVideoReference(ModuleConfiguration config, boolean strict) {
 		if (config == null) {
-			if (strict) throw new AssertException("missing config in Video");
-			else return null;
+			if (strict) {
+				throw new AssertException("missing config in Video");
+			}
+			return null;
 		}
 		String repoSoftkey = (String) config.get(VideoEditController.CONFIG_KEY_REPOSITORY_SOFTKEY);
 		if (repoSoftkey == null) {
-			if (strict) throw new AssertException("invalid config when being asked for references");
-			else return null;
+			if (strict) {
+				throw new AssertException("invalid config when being asked for references");
+			}
+			return null;
 		}
-		RepositoryManager rm = RepositoryManager.getInstance();
-		return rm.lookupRepositoryEntryBySoftkey(repoSoftkey, strict);
+		return RepositoryManager.getInstance().lookupRepositoryEntryBySoftkey(repoSoftkey, strict);
 	}
 
-	/**
-	 * @param moduleConfiguration
-	 * @return boolean
-	 */
 	public static boolean isModuleConfigValid(ModuleConfiguration moduleConfiguration) {
 		boolean isValid = moduleConfiguration.get(CONFIG_KEY_REPOSITORY_SOFTKEY) != null;
 		if (isValid) {
@@ -308,12 +327,6 @@ public class VideoEditController extends ActivateableTabbableDefaultController i
 		return isValid;
 	}
 
-	/**
-	 * Set the referenced repository entry.
-	 *
-	 * @param re
-	 * @param moduleConfiguration
-	 */
 	public static void setVideoReference(RepositoryEntry re, ModuleConfiguration moduleConfiguration) {
 		moduleConfiguration.set(CONFIG_KEY_REPOSITORY_SOFTKEY, re.getSoftkey());
 	}
