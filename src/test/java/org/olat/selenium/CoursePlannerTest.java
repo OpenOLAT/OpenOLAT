8dfde84d0d6390a2d6b4b84253929c97804c5ebc
srosse <stephane.rosse@frentix.com>
2025-09-23 10:13:18 +0200

    OO-8933: ignore selenium test

--- src/test/java/org/olat/selenium/CoursePlannerTest.java
+++ src/test/java/org/olat/selenium/CoursePlannerTest.java
@@ -28,6 +28,7 @@ import java.util.UUID;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.olat.modules.curriculum.CurriculumElementStatus;
@@ -370,6 +371,7 @@ public class CoursePlannerTest extends Deployments {
 	 * @throws URISyntaxException
 	 */
 	@Test
+	@Ignore
 	@RunAsClient
 	public void userBookingFromWebCatalog()
 	throws IOException, URISyntaxException {
