8c152db47a12f6db6788a4cd71ae88afb28677fb
cpfranger <christoph.pfranger@frentix.com>
2025-12-11 17:38:21 +0100

    OO-6096: Allow elements to be moved by clicking, similar to the drag & drop question type
    (cherry picked from commit 0e7a44b1fab6c17aa50c38dc3825edd6478a74f2)

--- src/main/webapp/static/js/jquery/qti/jquery.order.js
+++ src/main/webapp/static/js/jquery/qti/jquery.order.js
@@ -52,6 +52,119 @@
 		}).on('out', function(el) {
     		jQuery(targetContainer).removeClass('oo-accepted');
 		});
+		
+		initializeSourceClick(sourceList, targetList);
+		initializeTargetClick(sourceList, targetList, settings);
+	}
+	
+	function initializeSourceClick(sourceList, targetList) {
+		var sources = sourceList.find('li');
+		sources.on('click', function(e, el) {
+			handleSourceClick(jQuery(this), sourceList, targetList);
+		});
+	}
+
+	function handleSourceClick(listItem, sourceList, targetList) {
+		if (!listItem.hasClass('oo-selected')) {
+			unselectAllItems(sourceList, targetList);
+			listItem.addClass('oo-selected');
+			targetList.addClass('oo-selected');
+		} else {
+			unselectAllItems(sourceList, targetList);
+			targetList.removeClass('oo-selected');
+		}
+	}
+
+	function initializeTargetClick(sourceList, targetList, settings) {
+		targetList.on('click', function(e, el) { 
+			if (e.target === targetList.get(0)) {
+				handleTargetBackgroundClick(e, sourceList, targetList, settings);
+			} else {
+				handleTargetClick(e.target, sourceList, targetList);
+			}
+		});
+	}
+	
+	function handleTargetClick(clickedEl, sourceList, targetList) {
+		var listItem = jQuery(clickedEl).closest('li.o_assessmentitem_order_item');
+		if (listItem.length > 0) {
+			if (!listItem.hasClass('oo-selected')) {
+				unselectAllItems(sourceList, targetList);
+				listItem.addClass('oo-selected');
+				targetList.addClass('oo-selected');
+			} else {
+				unselectAllItems(sourceList, targetList);
+				targetList.removeClass('oo-selected');
+			}
+		}
+	}
+	
+	function unselectAllItems(sourceList, targetList) {
+		var targets = targetList.find('li');
+		targets.each(function(index, el) {
+			jQuery(el).removeClass('oo-selected');
+		});
+		var sources = sourceList.find('li');
+		sources.each(function(index, el) {
+			jQuery(el).removeClass('oo-selected');
+		});
+	}
+	
+	function handleTargetBackgroundClick(e, sourceList, targetList, settings) {
+		var selectedSourceEl = sourceList.find('li.oo-selected');
+		var selectedTargetEl = targetList.find('li.oo-selected');
+		var selectedEl = null;
+		if (selectedSourceEl.length > 0) {
+			selectedEl = selectedSourceEl;
+		} else if (selectedTargetEl.length > 0) {
+			selectedEl = selectedTargetEl;
+		}
+		if (!(selectedEl)) {
+			return;
+		}
+		selectedEl.removeClass('oo-selected');
+		
+		var selectedId = selectedEl.attr('id');
+
+		var listItems = targetList.find('li');
+		var duplicateListItem = targetList.find('li#' + selectedId);
+		
+		if (listItems.length === 0) {
+			var clone = selectedEl.clone();
+			targetList.append(clone);
+			targetList.removeClass('oo-selected');
+			selectedSourceEl.remove();
+			recalculate(settings);
+			return;
+		}
+		
+		var targetListRect = targetList.get(0).getBoundingClientRect();
+		var yHit = e.offsetY;
+		var y0 = 0;
+		var y1 = 0;
+		var inserted = false;
+		for (var i = 0; i < listItems.length; i++) {
+			var listItem = listItems.get(i);
+			var rect = listItem.getBoundingClientRect();
+			y1 = rect.y - targetListRect.y;
+			if (yHit >= y0 && yHit < y1) {
+				jQuery(listItem).before(selectedEl.clone());
+				inserted = true;
+				break;
+			}
+			y0 = y1 + rect.height;
+		}
+		if (!inserted) {
+			targetList.append(selectedEl.clone());
+		}
+		targetList.removeClass('oo-selected');
+		
+		if (duplicateListItem.length > 0) {
+			duplicateListItem.remove();
+		}
+
+		selectedSourceEl.remove();
+		recalculate(settings);
 	}
 
 	function recalculate(settings) {
