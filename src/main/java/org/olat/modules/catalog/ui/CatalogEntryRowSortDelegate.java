3de43d69696c7ee6c067352a7a190cca6ff0085a
uhensler <urs.hensler@frentix.com>
2026-01-13 14:12:08 +0100

    OO-9158: Fine tuning of sort in catalog

--- src/main/java/org/olat/modules/catalog/ui/CatalogEntryRowSortDelegate.java
+++ src/main/java/org/olat/modules/catalog/ui/CatalogEntryRowSortDelegate.java
@@ -21,12 +21,14 @@ package org.olat.modules.catalog.ui;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 import org.olat.core.commons.persistence.SortKey;
 import org.olat.core.gui.components.form.flexible.impl.elements.table.SortableFlexiTableModelDelegate;
-import org.olat.modules.catalog.ui.CatalogEntryDataModel.CatalogEntryCols;
+import org.olat.core.util.DateUtils;
+
 
 /**
  * 
@@ -48,10 +50,66 @@ public class CatalogEntryRowSortDelegate extends SortableFlexiTableModelDelegate
 		}
 		
 		int columnIndex = getColumnIndex();
-		CatalogEntryCols column = CatalogEntryCols.values()[columnIndex];
-		switch(column) {
+		switch(CatalogEntryDataModel.COLS[columnIndex]) {
 			case title: Collections.sort(rows, (r1, r2) -> compareString(r1.getTitle(), r2.getTitle())); break;
+			case lifecycleStart: Collections.sort(rows, new BeginDateComparator()); break;
+			case lifecycleEnd: Collections.sort(rows, new EndDateComparator()); break;
 			default: super.sort(rows);
+		}
+	}
+	
+	@Override
+	protected void reverse(List<CatalogEntryRow> rows) {
+		int columnIndex = getColumnIndex();
+		if(columnIndex >= 0 && columnIndex < CatalogEntryDataModel.COLS.length) {
+			switch(CatalogEntryDataModel.COLS[columnIndex]) {
+				case lifecycleStart, lifecycleEnd: break;
+				default: super.reverse(rows); break;
+			}
+		} else {
+			super.reverse(rows);
+		}
+	}
+	
+	private int compareNullLast(Object o1, Object o2) {
+		if(o1 == null && o2 != null) {
+			return 1;
+		}
+		if(o1 != null && o2 == null) {
+			return -1;
+		}
+		return 0;
+	}
+	
+	private class BeginDateComparator implements Comparator<CatalogEntryRow> {
+		@Override
+		public int compare(CatalogEntryRow o1, CatalogEntryRow o2) {
+			if(o1 == null || o2 == null) {
+				return compareNullObjects(o1, o2);
+			}
+			Date d1 = DateUtils.getStartOfDay(o1.getLifecycleStart());
+			Date d2 = DateUtils.getStartOfDay(o2.getLifecycleStart());
+			if (d1 == null || d2 == null) {
+				return compareNullLast(d1, d2);
+			}
+			int c = compareDateAndTimestamps(d1, d2, true);
+			return isAsc() ? c : -c;
+		}
+	}
+	
+	private class EndDateComparator implements Comparator<CatalogEntryRow> {
+		@Override
+		public int compare(CatalogEntryRow o1, CatalogEntryRow o2) {
+			if(o1 == null || o2 == null) {
+				return compareNullObjects(o1, o2);
+			}
+			Date d1 = DateUtils.getStartOfDay(o1.getLifecycleEnd());
+			Date d2 = DateUtils.getStartOfDay(o2.getLifecycleEnd());
+			if (d1 == null || d2 == null) {
+				return compareNullLast(d1, d2);
+			}
+			int c = compareDateAndTimestamps(d1, d2, true);
+			return isAsc() ? c : -c;
 		}
 	}
 	
@@ -61,6 +119,14 @@ public class CatalogEntryRowSortDelegate extends SortableFlexiTableModelDelegate
 		public int compare(CatalogEntryRow o1, CatalogEntryRow o2) {
 			// Higher priority is before lower priority
 			int c = -o1.getSortPriority().compareTo(o2.getSortPriority());
+			
+			if (c == 0) {
+				c = compareDateAndTimestamps(DateUtils.getStartOfDay(o1.getLifecycleStart()), DateUtils.getStartOfDay(o2.getLifecycleStart()), false);
+			}
+			
+			if (c == 0) {
+				c = compareDateAndTimestamps(DateUtils.getStartOfDay(o1.getLifecycleEnd()), DateUtils.getStartOfDay(o2.getLifecycleEnd()), false);
+			}
 			
 			if (c == 0) {
 				c = compareString(o1.getTitle(), o2.getTitle());
