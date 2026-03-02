0c9c8abec8f7bfe45f170fd59c2fe6e50598bceb
srosse <stephane.rosse@frentix.com>
2026-03-02 07:39:17 +0100

    OO-9123: update QtiWorks

--- src/test/java/org/olat/core/commons/services/ai/AiModuleTest.java
+++ src/test/java/org/olat/core/commons/services/ai/AiModuleTest.java
@@ -25,7 +25,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
