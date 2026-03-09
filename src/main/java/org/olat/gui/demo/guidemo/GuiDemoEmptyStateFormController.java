92245158707da03a50f7dbe8a26e7b34bb6771af
cpfranger <christoph.pfranger@frentix.com>
2026-03-09 17:27:02 +0100

    OO-7557: Add FlexiTable with empty state example to demo

--- src/main/java/org/olat/gui/demo/guidemo/GuiDemoEmptyStateFormController.java
+++ src/main/java/org/olat/gui/demo/guidemo/GuiDemoEmptyStateFormController.java
@@ -19,19 +19,30 @@
  */
 package org.olat.gui.demo.guidemo;
 
+import java.util.List;
+
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.components.emptystate.EmptyState;
 import org.olat.core.gui.components.emptystate.EmptyStateConfig;
 import org.olat.core.gui.components.emptystate.EmptyStateFactory;
 import org.olat.core.gui.components.form.flexible.FormItemContainer;
+import org.olat.core.gui.components.form.flexible.elements.FlexiTableElement;
 import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
+import org.olat.core.gui.components.form.flexible.impl.elements.table.DefaultFlexiColumnModel;
+import org.olat.core.gui.components.form.flexible.impl.elements.table.DefaultFlexiTableDataModel;
+import org.olat.core.gui.components.form.flexible.impl.elements.table.FlexiSortableColumnDef;
+import org.olat.core.gui.components.form.flexible.impl.elements.table.FlexiTableColumnModel;
+import org.olat.core.gui.components.form.flexible.impl.elements.table.FlexiTableDataModelFactory;
 import org.olat.core.gui.control.Controller;
 import org.olat.core.gui.control.WindowControl;
 
 /**
- * Demo of an empty state embedded inside a FormBasicController.
+ * Demo of an empty state embedded inside a FormBasicController,
+ * including a FlexiTable with its own empty state.
  */
 public class GuiDemoEmptyStateFormController extends FormBasicController {
+
+	private FlexiTableElement tableEl;
 
 	public GuiDemoEmptyStateFormController(UserRequest ureq, WindowControl wControl) {
 		super(ureq, wControl, "guidemo-empty-state-form");
@@ -49,6 +60,17 @@ public class GuiDemoEmptyStateFormController extends FormBasicController {
 		EmptyState emptyState = EmptyStateFactory.create("emptyStateInForm", flc.getFormItemComponent(), this, emptyStateConfig);
 		emptyState.setTranslator(getTranslator());
 
+		FlexiTableColumnModel columnsModel = FlexiTableDataModelFactory.createFlexiTableColumnModel();
+		columnsModel.addFlexiColumnModel(new DefaultFlexiColumnModel(Cols.item));
+		columnsModel.addFlexiColumnModel(new DefaultFlexiColumnModel(Cols.price));
+
+		ItemPriceTableModel tableModel = new ItemPriceTableModel(columnsModel);
+		tableModel.setObjects(List.of());
+
+		tableEl = uifactory.addTableElement(getWindowControl(), "itemPriceTable", tableModel, getTranslator(), formLayout);
+		tableEl.setEmptyTableSettings("empty.state.table.message", null, "o_icon_empty_objects",
+				"empty.state.button.primary.action", "o_icon_bolt_lightning", false);
+
 		uifactory.addFormSubmitButton("submit", formLayout);
 	}
 
@@ -56,4 +78,48 @@ public class GuiDemoEmptyStateFormController extends FormBasicController {
 	protected void formOK(UserRequest ureq) {
 		// nothing to do in demo
 	}
+
+	enum Cols implements FlexiSortableColumnDef {
+		item("table.col.item"),
+		price("table.col.price");
+
+		private final String i18nKey;
+
+		Cols(String i18nKey) {
+			this.i18nKey = i18nKey;
+		}
+
+		@Override
+		public String i18nHeaderKey() {
+			return i18nKey;
+		}
+
+		@Override
+		public boolean sortable() {
+			return false;
+		}
+
+		@Override
+		public String sortKey() {
+			return name();
+		}
+	}
+
+	private static class ItemPriceTableModel extends DefaultFlexiTableDataModel<ItemPriceRow> {
+
+		ItemPriceTableModel(FlexiTableColumnModel columnModel) {
+			super(columnModel);
+		}
+
+		@Override
+		public Object getValueAt(int row, int col) {
+			ItemPriceRow item = getObject(row);
+			return switch (Cols.values()[col]) {
+				case item -> item.item();
+				case price -> item.price();
+			};
+		}
+	}
+
+	record ItemPriceRow(String item, String price) {}
 }
