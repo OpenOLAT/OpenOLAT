78f57a6812b9f3cb1b6895283b091f30b256e48c
cpfranger <christoph.pfranger@frentix.com>
2026-06-01 17:51:47 +0200

    OO-9522: Add maps feature to EditBuildingController

--- src/main/java/org/olat/modules/roommanagement/ui/EditBuildingController.java
+++ src/main/java/org/olat/modules/roommanagement/ui/EditBuildingController.java
@@ -19,17 +19,29 @@
  */
 package org.olat.modules.roommanagement.ui;
 
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.olat.basesecurity.OrganisationModule;
 import org.olat.basesecurity.OrganisationRoles;
 import org.olat.basesecurity.OrganisationService;
 import org.olat.core.commons.services.color.ColorUIFactory;
 import org.olat.core.commons.services.color.ColorService;
+import org.olat.core.dispatcher.impl.StaticMediaDispatcher;
 import org.olat.core.gui.UserRequest;
 import org.olat.core.gui.components.form.flexible.FormItem;
 import org.olat.core.gui.components.form.flexible.FormItemContainer;
 import org.olat.core.gui.components.form.flexible.elements.ColorPickerElement;
+import org.olat.core.gui.components.form.flexible.elements.FormLink;
 import org.olat.core.gui.components.form.flexible.elements.FormToggle;
 import org.olat.core.gui.components.form.flexible.elements.TextAreaElement;
 import org.olat.core.gui.components.form.flexible.elements.TextElement;
@@ -37,15 +49,18 @@ import org.olat.core.gui.components.form.flexible.impl.FormBasicController;
 import org.olat.core.gui.components.form.flexible.impl.FormEvent;
 import org.olat.core.gui.components.form.flexible.impl.FormLayoutContainer;
 import org.olat.core.gui.components.form.flexible.impl.elements.ObjectSelectionElement;
+import org.olat.core.gui.components.htmlheader.jscss.JSAndCSSComponent;
 import org.olat.core.gui.components.link.Link;
 import org.olat.core.gui.control.Controller;
 import org.olat.core.gui.control.Event;
 import org.olat.core.gui.control.WindowControl;
 import org.olat.core.id.Organisation;
 import org.olat.core.id.Roles;
+import org.olat.core.logging.Tracing;
 import org.olat.core.util.StringHelper;
 import org.olat.modules.roommanagement.Building;
 import org.olat.modules.roommanagement.RoomManagementService;
+import org.olat.core.util.httpclient.HttpClientService;
 import org.olat.user.ui.organisation.OrganisationSelectionSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -55,17 +70,23 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class EditBuildingController extends FormBasicController {
 
+	private static final Logger log = Tracing.createLoggerFor(EditBuildingController.class);
+
 	private TextElement referenceEl;
 	private TextElement descriptionEl;
 	private ColorPickerElement colorEl;
 	private FormToggle orgRestrictionToggle;
 	private ObjectSelectionElement adminAccessEl;
+	private FormLayoutContainer addressMapCombo;
 	private TextElement addressEl;
 	private TextElement infoUrlEl;
 	private TextAreaElement additionalInfoEl;
+	private FormLink findOnMapLink;
 
 	private Building building;
 	private final List<Organisation> currentOrgs;
+	private BigDecimal geoLatitude;
+	private BigDecimal geoLongitude;
 
 	@Autowired
 	private RoomManagementService roomManagementService;
@@ -75,6 +96,8 @@ public class EditBuildingController extends FormBasicController {
 	private OrganisationModule organisationModule;
 	@Autowired
 	private OrganisationService organisationService;
+	@Autowired
+	private HttpClientService httpClientService;
 
 	public EditBuildingController(UserRequest ureq, WindowControl wControl) {
 		super(ureq, wControl, LAYOUT_VERTICAL);
@@ -88,6 +111,8 @@ public class EditBuildingController extends FormBasicController {
 		super(ureq, wControl, LAYOUT_VERTICAL);
 		this.building = building;
 		this.currentOrgs = organisations != null ? organisations : List.of();
+		geoLatitude = building.getGeoLatitude();
+		geoLongitude = building.getGeoLongitude();
 		initForm(ureq);
 	}
 
@@ -127,15 +152,24 @@ public class EditBuildingController extends FormBasicController {
 		locationCont.setFormTitle(translate("building.location"));
 		formLayout.add(locationCont);
 
-		FormLayoutContainer addressMapCombo = FormLayoutContainer.createCustomFormLayout("addressMapCombo", 
-				getTranslator(), this.velocity_root + "/address_map_combo.html");
+		String leafletCssUri = StaticMediaDispatcher.getStaticURI("js/leaflet/leaflet.css");
+		JSAndCSSComponent leafletLoader = new JSAndCSSComponent("leafletLoader",
+				new String[] { "js/leaflet/leaflet.min.js" },
+				new String[] { leafletCssUri });
+
+		addressMapCombo = FormLayoutContainer.createCustomFormLayout("addressMapCombo",
+				getTranslator(), velocity_root + "/address_map_combo.html");
 		locationCont.add(addressMapCombo);
-		
+		addressMapCombo.put("leafletLoader", leafletLoader);
+
 		addressEl = uifactory.addTextElement("building.address", "building.col.address", 1000,
 				building != null ? building.getAddress() : null, addressMapCombo);
 		addressEl.setMandatory(true);
 
-		uifactory.addFormLink("building.find.on.map", addressMapCombo, Link.BUTTON).setIconLeftCSS("o_icon o_icon_search");
+		findOnMapLink = uifactory.addFormLink("building.find.on.map", addressMapCombo, Link.BUTTON);
+		findOnMapLink.setIconLeftCSS("o_icon o_icon_search");
+
+		updateMapContext();
 
 		FormLayoutContainer infoCont = FormLayoutContainer.createVerticalFormLayout("information", getTranslator());
 		infoCont.setFormTitle(translate("building.information"));
@@ -151,6 +185,18 @@ public class EditBuildingController extends FormBasicController {
 		FormLayoutContainer buttonsCont = uifactory.addButtonsFormLayout("buttons", null, formLayout);
 		uifactory.addFormSubmitButton(building == null ? "create" : "save", buttonsCont);
 		uifactory.addFormCancelButton("cancel", buttonsCont, ureq, getWindowControl());
+	}
+
+	private void updateMapContext() {
+		if (geoLatitude != null && geoLongitude != null) {
+			addressMapCombo.contextPut("geoLat", geoLatitude);
+			addressMapCombo.contextPut("geoLon", geoLongitude);
+		} else {
+			addressMapCombo.contextRemove("geoLat");
+			addressMapCombo.contextRemove("geoLon");
+		}
+		ColorPickerElement.Color color = colorEl != null ? colorEl.getColor() : null;
+		addressMapCombo.contextPut("colorCss", color != null ? color.cssClass() : "");
 	}
 
 	@Override
@@ -184,6 +230,8 @@ public class EditBuildingController extends FormBasicController {
 	protected void formInnerEvent(UserRequest ureq, FormItem source, FormEvent event) {
 		if (source == orgRestrictionToggle) {
 			updateAdminAccessVisibility();
+		} else if (source == findOnMapLink) {
+			doFindOnMap();
 		}
 		super.formInnerEvent(ureq, source, event);
 	}
@@ -191,6 +239,49 @@ public class EditBuildingController extends FormBasicController {
 	private void updateAdminAccessVisibility() {
 		if (adminAccessEl != null) {
 			adminAccessEl.setVisible(orgRestrictionToggle.isOn());
+		}
+	}
+
+	private void doFindOnMap() {
+		String address = addressEl.getValue();
+		if (!StringHelper.containsNonWhitespace(address)) {
+			addressEl.setErrorKey("form.legende.mandatory");
+			return;
+		}
+		addressEl.clearError();
+
+		try {
+			String encoded = URLEncoder.encode(address.trim(), StandardCharsets.UTF_8);
+			String url = "https://nominatim.openstreetmap.org/search?q=" + encoded + "&format=json&limit=1";
+
+			HttpGet get = new HttpGet(url);
+			get.setHeader("User-Agent", "OpenOlat/RoomManagement (https://www.openolat.org)");
+			get.setHeader("Accept-Language", getLocale().getLanguage());
+
+			try (CloseableHttpClient client = httpClientService.createThreadSafeHttpClient(true);
+				 CloseableHttpResponse response = client.execute(get)) {
+
+				int status = response.getStatusLine().getStatusCode();
+				String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+
+				if (status == 200) {
+					JSONArray results = new JSONArray(body);
+					if (results.length() > 0) {
+						JSONObject first = results.getJSONObject(0);
+						geoLatitude  = new BigDecimal(first.getString("lat"));
+						geoLongitude = new BigDecimal(first.getString("lon"));
+						updateMapContext();
+					} else {
+						showWarning("building.geocoding.not.found");
+					}
+				} else {
+					log.warn("Nominatim returned HTTP {} for address: {}", status, address);
+					showWarning("building.geocoding.error");
+				}
+			}
+		} catch (Exception e) {
+			log.error("Geocoding failed for address: {}", address, e);
+			showWarning("building.geocoding.error");
 		}
 	}
 
@@ -205,6 +296,8 @@ public class EditBuildingController extends FormBasicController {
 		building.setAddress(addressEl.getValue());
 		building.setInfoUrl(infoUrlEl.getValue());
 		building.setInfo(additionalInfoEl.getValue());
+		building.setGeoLatitude(geoLatitude);
+		building.setGeoLongitude(geoLongitude);
 
 		ColorPickerElement.Color color = colorEl.getColor();
 		building.setColorCss(color != null ? color.cssClass() : null);
