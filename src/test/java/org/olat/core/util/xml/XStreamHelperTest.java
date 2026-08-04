731c259a3a3bf667efbf64d30105dd9798630654
uhensler <urs.hensler@frentix.com>
2026-08-04 10:17:33 +0200

    OO-9671: Remap Hibernate proxy class names globally in XStream

--- src/test/java/org/olat/core/util/xml/XStreamHelperTest.java
+++ src/test/java/org/olat/core/util/xml/XStreamHelperTest.java
@@ -20,12 +20,16 @@
 package org.olat.core.util.xml;
 
 import java.io.File;
+import java.lang.reflect.Proxy;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.proxy.LazyInitializer;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.security.ForbiddenClassException;
 
 /**
@@ -64,5 +68,73 @@ public class XStreamHelperTest {
 		URL url = XStreamHelperTest.class.getResource("xstream_map_alien.xml");
 		File file = new File(url.toURI());
 		XStreamHelper.createXStreamInstanceForDBObjects().fromXML(file);
+	}
+
+	@Test
+	public void readXmlHibernateProxyRemappedToBaseClass() {
+		String xml = "<org.olat.core.util.xml.XStreamHelperTest_-TestBean_-HibernateProxy_-a1b2c3d4><name>hello</name></org.olat.core.util.xml.XStreamHelperTest_-TestBean_-HibernateProxy_-a1b2c3d4>";
+		XStream xstream = XStreamHelper.createXStreamInstance();
+		XStreamHelper.allowDefaultPackage(xstream);
+		Object obj = xstream.fromXML(xml);
+		Assert.assertTrue(obj instanceof TestBean);
+		Assert.assertEquals("hello", ((TestBean)obj).getName());
+	}
+
+	@Test(expected = ForbiddenClassException.class)
+	public void readXmlHibernateProxyNotAllowed() {
+		String xml = "<java.lang.ProcessBuilder_-HibernateProxy_-a1b2c3d4></java.lang.ProcessBuilder_-HibernateProxy_-a1b2c3d4>";
+		XStream xstream = XStreamHelper.createXStreamInstance();
+		XStreamHelper.allowDefaultPackage(xstream);
+		xstream.fromXML(xml);
+	}
+
+	@Test
+	public void writeXmlHibernateProxyUnwrapped() {
+		TestBean bean = new TestBean();
+		bean.setName("hello");
+		LazyInitializer lazyInitializer = (LazyInitializer)Proxy.newProxyInstance(getClass().getClassLoader(),
+				new Class<?>[] { LazyInitializer.class },
+				(p, method, args) -> "getImplementation".equals(method.getName()) ? bean : null);
+		TestBeanProxy proxy = new TestBeanProxy(lazyInitializer);
+
+		String xml = XStreamHelper.createXStreamInstance().toXML(proxy);
+		Assert.assertTrue(xml.contains("TestBean"));
+		Assert.assertTrue(xml.contains("hello"));
+		Assert.assertFalse(xml.contains("HibernateProxy"));
+		Assert.assertFalse(xml.contains("TestBeanProxy"));
+	}
+
+	public static class TestBean {
+
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	public static class TestBeanProxy extends TestBean implements HibernateProxy {
+
+		private static final long serialVersionUID = -634817780480177319L;
+		
+		private final transient LazyInitializer lazyInitializer;
+
+		public TestBeanProxy(LazyInitializer lazyInitializer) {
+			this.lazyInitializer = lazyInitializer;
+		}
+
+		@Override
+		public Object writeReplace() {
+			return this;
+		}
+
+		@Override
+		public LazyInitializer getHibernateLazyInitializer() {
+			return lazyInitializer;
+		}
 	}
 }
