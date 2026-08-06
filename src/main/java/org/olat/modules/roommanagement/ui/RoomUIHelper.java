a476c2740e093c83b48ebda231fae7a7ce291431
cpfranger <christoph.pfranger@frentix.com>
2026-08-06 14:03:16 +0200

    OO-9641: Truncate room and building descriptions in filters and hide description if identical to reference

--- src/main/java/org/olat/modules/roommanagement/ui/RoomUIHelper.java
+++ src/main/java/org/olat/modules/roommanagement/ui/RoomUIHelper.java
@@ -57,7 +57,7 @@ import org.olat.modules.roommanagement.RoomStatus;
 public class RoomUIHelper {
 
 	private static final int MAX_COLUMN_INFO_TEXT_LENGTH = 250;
-
+	private static final int MAX_DESC_LENGTH = 40;
 	/**
 	 * Business path to the given lecture block in the curriculum admin "Events" list,
 	 * opening and expanding its row detail view there.
@@ -100,8 +100,9 @@ public class RoomUIHelper {
 		if (StringHelper.containsNonWhitespace(room.getExternalRef())) {
 			cardCont.contextPut("reference", room.getExternalRef());
 		}
-		if (StringHelper.containsNonWhitespace(room.getDescription())) {
-			cardCont.contextPut("description", room.getDescription());
+		String roomDesc = room.getDescription();
+		if (StringHelper.containsNonWhitespace(roomDesc) && !roomDesc.equals(room.getExternalRef())) {
+			cardCont.contextPut("description", roomDesc);
 		}
 
 		Building building = room.getBuilding();
@@ -203,14 +204,16 @@ public class RoomUIHelper {
 		if (!hasRef && !hasDesc) {
 			return null;
 		}
-
+		
+		boolean showDesc = hasDesc && !desc.equals(ref);
+		
 		StringBuilder html = new StringBuilder();
 		if (hasRef) {
 			html.append("<span>").append(StringHelper.escapeHtml(ref)).append("</span>");
 		}
-		if (hasDesc) {
+		if (hasDesc && showDesc) {
 			html.append("<span class=\"").append(descCssClass).append(" text-muted\"> &middot; ")
-					.append(StringHelper.escapeHtml(desc)).append("</span>");
+					.append(StringHelper.escapeHtml(Formatter.truncate(desc, MAX_DESC_LENGTH))).append("</span>");
 		}
 		html.append(buildStatusSuffixHtml(status, translator));
 		return html.toString();
