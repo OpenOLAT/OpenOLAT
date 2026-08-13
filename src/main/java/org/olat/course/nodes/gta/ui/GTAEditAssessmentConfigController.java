a0952aefbd9ee489f0f0d3ea73c4e4a5247c8cf7
srosse <stephane.rosse@frentix.com>
2026-08-13 16:53:00 +0200

    OO-9699: don't trust the min. value

--- src/main/java/org/olat/course/nodes/gta/ui/GTAEditAssessmentConfigController.java
+++ src/main/java/org/olat/course/nodes/gta/ui/GTAEditAssessmentConfigController.java
@@ -696,14 +696,14 @@ public class GTAEditAssessmentConfigController extends FormBasicController imple
 		maxValEl.clearError();
 		evaluationScoreScalingEl.clearError();
 		if (scoreGranted.isOn()) {
-			if (!minValEl.getValue().matches(scoreRex)) {
+			if (!isFloat(minValEl.getValue())) {
 				minValEl.setErrorKey("form.error.wrongFloat");
 				allOk &= false;
 			}
-			if (!maxValEl.getValue().matches(scoreRex)) {
+			if (!isFloat(maxValEl.getValue())) {
 				maxValEl.setErrorKey("form.error.wrongFloat");
 				allOk &= false;
-			} else if (Float.parseFloat(minValEl.getValue()) > Float.parseFloat(maxValEl.getValue())) {
+			} else if (isFloat(minValEl.getValue()) && Float.parseFloat(minValEl.getValue()) > Float.parseFloat(maxValEl.getValue())) {
 				maxValEl.setErrorKey("form.error.minGreaterThanMax");
 				allOk &= false;
 			}
@@ -754,7 +754,7 @@ public class GTAEditAssessmentConfigController extends FormBasicController imple
 						Float.parseFloat(value);
 					} catch(Exception e) {
 						el.setErrorKey("form.error.wrongFloat");
-						allOk = false;
+						allOk &= false;
 					}
 				} else {
 					try {
@@ -770,6 +770,18 @@ public class GTAEditAssessmentConfigController extends FormBasicController imple
 		}
 		
 		return allOk;
+	}
+	
+	private boolean isFloat(String val) {
+		if (StringHelper.containsNonWhitespace(val)) {
+			try {
+				Float.parseFloat(val);
+				return true;
+			} catch (NumberFormatException e) {
+				// 
+			}
+		}
+		return false;
 	}
 	
 	@Override
