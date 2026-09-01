4e99ca68e20aeeb147d020445007e2862b3548c3
cpfranger <christoph.pfranger@frentix.com>
2026-09-01 11:02:42 +0200

    OO-9717: MediaSite configuration errors are now detected in the course editor

--- src/main/java/org/olat/course/nodes/MediaSiteCourseNode.java
+++ src/main/java/org/olat/course/nodes/MediaSiteCourseNode.java
@@ -120,9 +120,36 @@ public class MediaSiteCourseNode extends AbstractAccessableCourseNode {
 			sd = new StatusDescription(StatusDescription.ERROR, shortKey, longKey, null, translPackage);
 			sd.setDescriptionForUnit(getIdent());
 			sd.setActivateableViewIdentifier(MediaSiteEditController.PANE_TAB_VCCONFIG);
+		} else if (isLti13Incomplete(mediaSiteModule, usesPrivateLogin)) {
+			String shortKey = "edit.warning.lti13.not.configured.short";
+			String longKey = "edit.warning.lti13.not.configured";
+			String translPackage = MediaSiteAdminController.class.getPackageName();
+
+			sd = new StatusDescription(StatusDescription.ERROR, shortKey, longKey, null, translPackage);
+			sd.setDescriptionForUnit(getIdent());
+			sd.setActivateableViewIdentifier(MediaSiteEditController.PANE_TAB_VCCONFIG);
 		}
 
 		return sd;
+	}
+
+	/**
+	 * Checks that the config keys needed to launch LTI 1.3 (see MediaSiteRunController.showContentLti13())
+	 * are present, without resolving them against the database - a course tree with many nodes can call
+	 * isConfigValid() repeatedly, so this stays a cheap, config-only check, same as the equivalent check
+	 * in BasicLTICourseNode.validateInternalConfiguration() for the generic LTI 1.3 course element.
+	 */
+	private boolean isLti13Incomplete(MediaSiteModule mediaSiteModule, boolean usesPrivateLogin) {
+		if (!LtiVersion.lti_1_3.name().equals(getModuleConfiguration().getStringValue(CONFIG_LTI_VERSION))) {
+			return false;
+		}
+		if (usesPrivateLogin) {
+			String toolKey = getModuleConfiguration().getStringValue(CONFIG_LTI13_TOOL_KEY);
+			String baseUrl = getModuleConfiguration().getStringValue(CONFIG_LTI13_BASE_URL);
+			return !StringHelper.isLong(toolKey) || !StringHelper.containsNonWhitespace(baseUrl);
+		}
+		return mediaSiteModule.getLti13DeploymentKey() == null
+				|| !StringHelper.containsNonWhitespace(mediaSiteModule.getLti13BaseUrl());
 	}
 	
 	@Override
